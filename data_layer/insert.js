7a20d86292532b9a11b4fa27917868342babd7d0
Kyle Bernhardy <kyle@harperdb.io>
2017-03-04 23:27:46 -0700

    Bulk load & code restructure
    Implemented bulkk load, restructured code, added initial libraries for
    hiding old versions of files

--- data_layer/insert.js
+++ data_layer/insert.js
@@ -2,10 +2,26 @@ const insert_validator = require('../validation/insertValidator.js'),
     fs = require('fs'),
     async = require('async'),
     path = require('path'),
-    settings = require('settings');
+    settings = require('settings'),
+    hidefile = require('hidefile'),
+    glob = require('glob');
 
-const hdb_path = settings.PROJECT_DIR + '\\hdb\\schema';
+const relative_path = path.relative(__dirname, settings.PROJECT_DIR);
+const hdb_path = relative_path + '/hdb/schema';
+
 module.exports = {
+    bulkInsert:function(insert_array, callback){
+        var insert = this.insert;
+        insert_array.forEach(function(insert_object){
+            insert(insert_object, function(err, data){
+                if(err){
+                    callback(err);
+                    return;
+                }
+                callback();
+            });
+        });
+    },
     insert: function (insert_object, callback) {
         //validate insert_object for required attributes
         var validator = insert_validator(insert_object);
@@ -15,8 +31,8 @@ module.exports = {
         }
 
         //check if schema / table directories exist
-        var table_path = hdb_path + '\\' + insert_object.schema + '\\' + insert_object.table;
-        if (!this.checkPathExists(table_path)) {
+        var table_path = hdb_path + '/' + insert_object.schema + '/' + insert_object.table;
+        if (!checkPathExists(table_path)) {
             callback('Table: ' + insert_object.schema + '.' + insert_object.table + ' does not exist');
             return;
         }
@@ -24,89 +40,102 @@ module.exports = {
         //verify hash_attribute is correct for this table
 
         //deconstruct object into seperate peices
-        var attribute_array = this.deconstructObject(insert_object);
-        console.log(attribute_array);
-        this.insertObject(attribute_array);
+        var attribute_array = deconstructObject(insert_object);
+
+        insertObject(attribute_array);
 
         callback();
-    },
-    checkPathExists: function (path) {
-        return fs.existsSync(path);
-    },
+    }
+};
 
-    deconstructObject: function (insert_object) {
-        var attribute_array = [];
+function checkPathExists (path) {
+    return fs.existsSync(path);
+}
 
-        //add hash
-        attribute_array.push(this.createAttributeObject(insert_object, insert_object.hash_attribute));
+function deconstructObject(insert_object) {
+    var attribute_array = [];
 
-        for (var property in insert_object.object) {
-            if (insert_object.object.hasOwnProperty(property)) {
-                attribute_array.push(this.createAttributeObject(insert_object, property));
-            }
+    //add hash
+    attribute_array.push(createAttributeObject(insert_object, insert_object.hash_attribute));
+
+    for (var property in insert_object.object) {
+        if (insert_object.object.hasOwnProperty(property)) {
+            attribute_array.push(createAttributeObject(insert_object, property));
         }
+    }
 
-        return attribute_array;
-    },
+    return attribute_array;
+}
 
-    createAttributeObject: function (insert_object, attribute_name) {
-        var attribute_object = {
-            schema: insert_object.schema,
-            table: insert_object.table,
-            attribute_name: attribute_name,
-            attribute_value: attribute_name === insert_object.hash_attribute ? insert_object.hash_value : insert_object.object[attribute_name],
-            hash_value: insert_object.hash_value,
-            is_hash: attribute_name === insert_object.hash_name ? true : false
-        };
+function createAttributeObject(insert_object, attribute_name) {
+    var attribute_object = {
+        schema: insert_object.schema,
+        table: insert_object.table,
+        attribute_name: attribute_name,
+        attribute_value: attribute_name === insert_object.hash_attribute ? insert_object.hash_value : insert_object.object[attribute_name],
+        hash_value: insert_object.hash_value,
+        is_hash: attribute_name === insert_object.hash_attribute ? true : false
+    };
 
-        return attribute_object;
-    },
+    return attribute_object;
+}
 
-    checkAttributeSchema: function (attribute) {
-        var attribute_path = hdb_path + '\\' + attribute.schema + '\\' + attribute.table + '\\' + attribute.attribute_name;
-        if (!this.checkPathExists(attribute_path)) {
+function checkAttributeSchema(attribute) {
+    var attribute_path = hdb_path + '/' + attribute.schema + '/' + attribute.table + '/' + attribute.attribute_name;
+    if (!checkPathExists(attribute_path)) {
+        //need to write new attribute to the hdb_attribute table
+        fs.mkdirSync(attribute_path);
+    }
+}
+
+function insertObject(attribute_array) {
+    //if attribute is new create atribute folder
+
+    // insert record into /table/attribute/value-timestamp-hash.hdb
+
+    async.each(attribute_array, function (attribute, callback) {
+        //compare object attributes to known schema, if new attributes add to system.hdb_attribute table
+        var attribute_path = hdb_path + '/' + attribute.schema + '/' + attribute.table + '/' + attribute.attribute_name;
+        if (!checkPathExists(attribute_path)) {
             //need to write new attribute to the hdb_attribute table
             fs.mkdirSync(attribute_path);
         }
-    },
-
-    insertObject: function (attribute_array) {
-        //if attribute is new create atribute folder
-        console.log(attribute_array);
-        // insert record into /table/attribute/value-timestamp-hash.hdb
-        var checkPathExists = this.checkPathExists;
-        var createAttributeValueFile = this.createAttributeValueFile;
-        async.each(attribute_array, function (attribute, callback) {
-            //compare object attributes to known schema, if new attributes add to system.hdb_attribute table
-            var attribute_path = hdb_path + '\\' + attribute.schema + '\\' + attribute.table + '\\' + attribute.attribute_name;
-            if (!checkPathExists(attribute_path)) {
-                //need to write new attribute to the hdb_attribute table
-                fs.mkdirSync(attribute_path);
-            }
-            createAttributeValueFile(attribute, function (err, data) {
-                if (err) {
-                    callback(err);
-                } else {
-                    //TODO mark pre - existing attribute value file for grep exclusion
-                    callback();
-                }
-            });
-        }, function (err) {
-
-        });
-    },
-
-    createAttributeValueFile: function (attribute, callback) {
-        var attribute_path = hdb_path + '\\' + attribute.schema + '\\' + attribute.table + '\\' + attribute.attribute_name + '\\';
-
-        var value_stripped = attribute.attribute_value.replace(/[^0-9a-z]/gi, '').substring(0, 206);
-        var attribute_file_name = attributevalue_stripped + '-' + new Date().getTime() + '-' + attribute.hash_value + '.hdb';
-        fs.writeFile(attribute_path + attribute_file_name, attribute.attribute_value, function (err, data) {
+        createAttributeValueFile(attribute, function (err, data) {
             if (err) {
                 callback(err);
             } else {
-                callback(data);
+                //TODO mark pre - existing attribute value file for grep exclusion
+                callback();
             }
         });
-    }
-};
+    }, function (err) {
+
+    });
+}
+
+function createAttributeValueFile(attribute, callback) {
+    var attribute_path = hdb_path + '/' + attribute.schema + '/' + attribute.table + '/' + attribute.attribute_name + '/';
+
+    var value_stripped = String(attribute.attribute_value).replace(/[^0-9a-z]/gi, '').substring(0, 206);
+    var attribute_file_name = attribute.is_hash ? attribute.hash_value + '.hdb' :
+        value_stripped + '-' + new Date().getTime() + '-' + attribute.hash_value + '.hdb';
+    fs.writeFile(attribute_path + attribute_file_name, attribute.attribute_value, function (err, data) {
+        if (err) {
+            callback(err);
+        } else {
+            if(!attribute.is_hash) {
+                glob('*-' + attribute.hash_value + '.hdb', {cwd: attribute_path}, function (err, d) {
+                    if (err) {
+                        console.error(err);
+                    } else {
+                        console.log(d);
+                      //  d.forEach(function)
+
+                    }
+                });
+            }
+            callback(data);
+
+        }
+    });
+}
--- test/insertTest.js
+++ test/insertTest.js
@@ -1,17 +1,33 @@
-var insert = require('../data_layer/insert.js');
-var settings = require('settings');
+var insert = require('../data_layer/insert.js'),
+    settings = require('settings'),
+    first_names = require('./firstNames'),
+    last_names = require('./lastNames'),
+    glob = require('glob');
 
-const insert_object = {
-    schema :  'dev',
-    table:'person',
-    hash_attribute: 'id',
-    hash_value : '123',
-    object:{
-        first_name : 'Kyle',
-        last_name: 'Bernhardy'
+/*glob('*-1.hdb', {cwd: '../hdb/schema/dev/person/first_name/', nodir:true}, function(err, data){
+    if(err){
+        console.error(err);
+    } else {
+        console.log(data);
     }
-};
+});*/
+var objects = [];
+for(var x = 0; x < 10000; x++){
 
-insert.insert(insert_object, function(err, data){
+    objects.push(
+        {
+            schema :  'dev',
+            table:'person',
+            hash_attribute: 'id',
+            hash_value : x+1,
+            object:{
+                first_name : first_names[Math.floor(Math.random() * first_names.length)],
+                last_name: last_names[Math.floor(Math.random() * last_names.length)]
+            }
+        }
+    );
+}
+
+insert.bulkInsert(objects, function(err, data){
     console.error(err);
 });
