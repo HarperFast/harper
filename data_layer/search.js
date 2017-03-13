6286125b9dbaa66c1ac9bd90e6ff1426b8a69b16
Stephen Goldberg <stephen@harperdb.io>
2017-03-12 19:51:40 -0600

    changed the search to use cd
    changed search to use cd then find instead of find with path option.
    Cut time in half.

--- data_layer/search.js
+++ data_layer/search.js
@@ -5,6 +5,7 @@ const base_path = path.join(settings.HDB_ROOT, "hdb/schema/");
 const exec = require('child_process').exec;
 const search_validator = require('../validation/searchValidator.js');
 async = require('async');
+console.time('whole test');
 
 
 // search by hash only
@@ -16,68 +17,65 @@ async = require('async');
 //schema, table, hash_value, hash_attribute, get_attributues, callback
 function searchByHash(search_object, callback) {
 
+
     var validation_error =search_validator(search_object);
     if (validation_error) {
         callback(validation_error, null);
         return;
     }
-    var hash_path = path.join(base_path,
+
+
+
+    var hash_path =  path.join(base_path,
         search_object.schema + '/' + search_object.table + '/' + search_object.hash_attribute + '/');
 
-    fs.readFile(hash_path + search_object.hash_value + '.hdb', function(err, data){
-       if(err){
-           if (validation_error) {
-               callback(validation_error, null);
-               return;
-           }
-
-           if (!fs.existsSync(base_path + search_object.schema)) {
-               callback("schema does not exist");
-               return;
-           }
-
-
-           if (!fs.existsSync(path.join(base_path, search_object.schema + "/" + search_object.table))) {
-               callback("table does not exist");
-               return;
-
-           }
-           if (!fs.existsSync(path.join(base_path, search_object.schema + "/"
-                   + search_object.table + "/" + search_object.hash_attribute))) {
-               callback("hash_atrribute does not exist");
-               return;
-
-           }
-           callback('id does not exist');
-           return;
-
-
+    fs.readFile(hash_path + search_object.hash_value + '.hdb', 'utf8', function(err, data){
+         if(err){
+            handleError();
+            return;
        }
        var object = {};
-       object[search_object.hash_attribute] = search_obj.hash_value;
-       async.map(search_object.get_attributes,
+       object[search_object.hash_attribute] = data;
+
+       var base_attr_path = path.join(base_path,
+            search_object.schema + '/' + search_object.table + '/');
+
+        console.time('asyncStart');
+        async.map(search_object.get_attributes,
             function (attribute, caller) {
+                console.timeEnd('asyncStart');
                 if (attribute == search_object.hash_attribute) {
                     caller();
                     return;
                 }
-                var attr_path = path.join(base_path,
-                    search_object.schema + '/' + search_object.table + '/' + attribute);
+
+
+
+
+                var attr_path = base_attr_path + attribute;
+
+
                 if (!fs.existsSync(attr_path)) {
                     caller('attribute does not exist');
                     return;
                 }
 
-                console.log('find ' + attr_path + ' -path  *-' + search_object.hash_value + '.hdb')
-                exec('find ' + attr_path + ' -path  *-' + search_object.hash_value + '.hdb', function (error, stdout, stderr) {
-                    if (error) {
+                console.time(attribute +' find command');
+                console.log('cd  ' + attr_path + '; find ./ -iname \'*-' + search_object.hash_value + '.hdb\'')
+                // by using cd to get to the directory instead of providing the path as part of the find command the overall time drops by half for the find.
+
+                exec('cd  ' + attr_path + '; find ./ -iname \'*-' + search_object.hash_value + '.hdb\'', function (error, stdout, stderr) {
+                    console.timeEnd(attribute +' find command');
+                     if (error) {
                         caller(error);
                         return;
                     }
 
-                    var tokens = stdout.replace('.hdb', '').split('/');
-                    object[attribute] = tokens[tokens.length - 1].split('-')[0];
-                    caller();
+                        fs.readFile(path.join(attr_path,  stdout.replace('\n', '').replace('.//', '')), 'utf8', function(err, data){
+                            object[attribute] = data;
+                            caller();
+                        });
+
 
 
                 });
@@ -90,30 +88,55 @@ function searchByHash(search_object, callback) {
 
     });
 
+    function handleError(){
 
+        if (!fs.existsSync(base_path + search_object.schema)) {
+            callback("schema does not exist");
+            return;
+        }
+
+
+        if (!fs.existsSync(path.join(base_path, search_object.schema + "/" + search_object.table))) {
+            callback("table does not exist");
+            return;
+
+        }
+        if (!fs.existsSync(path.join(base_path, search_object.schema + "/"
+                + search_object.table + "/" + search_object.hash_attribute))) {
+            callback("hash_atrribute does not exist");
+            return;
+
+        }
+        callback(null, null);
+        return;
+
+    }
 
 
 
 
 }
 
-var moment = require('moment')
+
 
 var search_obj = {};
 
 search_obj.schema = 'dev';
 search_obj.table = 'person';
 search_obj.hash_attribute = 'id';
-search_obj.hash_value = '9';
-search_obj.get_attributes = ['id', 'first_name', 'last_name']
-
-var start = process.hrtime();
+search_obj.hash_value = '1';
+search_obj.get_attributes = ['first_name', 'last_name', 'id']
 
 searchByHash(search_obj, function (err, result) {
-    console.error(err);
+
+    if(err)
+        console.error(err);
     console.log(result);
-    console.log(moment().format() + ' END!');
-    var diff = process.hrtime(start);
-    console.log(`finished took ${(diff[0] * 1e9 + diff[1]) / 1e9} seconds`);
+    console.timeEnd('whole test');
+
+
+
+
+
 
 })
