6fdec616e3c9cc39e47515efaf4c58c71b29dce6
zachary@harperdb.io <zachary@harperdb.io>
2017-04-06 15:41:10 -0600

    not working still

--- utility/devops/config.sh
+++ utility/devops/config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 script_home=$(pwd)
+mv $script_home/node_modules/@harperdb/settings/ $script_home/node_modules/settings
+
 #echo $script_home
 sed -ie "/HDB_ADDRESS/ s:\:.*:\:\'0\.0\.0\.0\':" $script_home/settings.js 
 sed -ie "/HDB_ROOT/ s:__dirname.*:\'$(find /opt -name hdb)\',:" $script_home/settings.js
-mv $script_home/node_modules/@harperdb/settings $script_home/node_modules/settings
