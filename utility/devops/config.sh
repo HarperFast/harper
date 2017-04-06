883a453aa083aa965135ef8db6deade2f3d28ba9
zachary@harperdb.io <zachary@harperdb.io>
2017-04-06 11:07:45 -0600

    updated post install deploy script

---
+++ utility/devops/config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+script_home=$(pwd)
+#echo $script_home
+sed -ie "/HDB_ADDRESS/ s:\:.*:\:\'0\.0\.0\.0\':" $script_home/settings.js 
+sed -ie "/HDB_ROOT/ s:__dirname.*:\'$(find /opt -name hdb)\',:" $script_home/settings.js
+
