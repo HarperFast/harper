8fbb9ad69e76e6bfb3ffb52a413a592a4f91984a
stephen goldberg <stephen@harperdb.io>
2017-04-17 16:22:08 -0600

    refactoring tcp server to spin up more threads.

--- server/index.js
+++ server/index.js
@@ -1,37 +1,104 @@
 #!/usr/bin/env node
 'use strict';
-    var method;
-    if(process.argv && process.argv.length > 2){
-        method =  process.argv[2];
-    }
-
-    console.log(method);
-    var port;
-    switch(method) {
-        case 'write':
-            port = 9925;
-            break;
-        case 'search':
-            port = 9926;
-            break;
-        case 'delete':
-            port = 9927;
-            break;
-        default:
-            port = 9926;
+    const     settings = require('settings'),
+        insert = require('../data_layer/insert.js'),
+        search  = require('../data_layer/search.js'),
+        hdb_delete = require('../data_layer/delete.js'),
+        max_data_size = 65536,
+        net = require('net');
+    var numPorts = settings.TCP_PORT_RANGE_END - settings.TCP_PORT_RANGE_BEGIN;
+    var counter =0;
+    while(counter < numPorts + 1){
+        let port  = settings.TCP_PORT_RANGE_BEGIN + counter;
+        net.createServer(conn).listen(port, settings.HDB_ADDRESS);
+        counter++;
     }
 
 
-// importing Server class
-    const Server = require('./../lib/server/Server'),
-        tcp_server = require('./../lib/server/tcp_server');
 
+function conn(socket){
+    let socket_data = '';
 
-
-    var server = new Server();
-
-// Starting our server
-    server.start(tcp_server,port, () => {
-        console.log('Server started on port ' + port);
+    socket.on('error', (err) => {
+        console.error(`Socket ${client.name} fail: ${err}`);
     });
+
+    socket.on('close', (err) => {
+        //console.log(`Socket ${client.name} disconnected`);
+    });
+
+    socket.on('data', onSocketData);
+
+    function onSocketData(data){
+        socket_data += data;
+
+        if(data.length <= max_data_size && isJson(socket_data)) {
+            let json = JSON.parse(socket_data);
+            if(!Object.keys(json)[0]){
+                socket.end('Missing operation');
+                return;
+            }
+
+            handleOperation(json, function(err, data){
+                if(err) {
+                    console.error(err);
+                    socket.end(JSON.stringify(err));
+                    return;
+                }
+                //console.log(`${client.name} ${data}`);
+
+                socket.end(JSON.stringify(data));
+                return;
+            });
+
+
+
+        }
+    }
+
+    function handleOperation(json, callback){
+        let payload = json[Object.keys(json)[0]];
+        console.log(payload);
+        switch(Object.keys(json)[0]){
+            case 'write':
+                insert.insert(payload, function(err, data){
+                    callback(err, data);
+                });
+                break;
+            case 'search_by_hash':
+                search.searchByHash(payload, function(err, data){
+                    callback(err, data);
+                });
+                break;
+            case 'search_by_hashes':
+                search.searchByHashes(payload, function(err, data){
+                    callback(err, data);
+                });
+                break;
+            case 'search_by_value':
+                search.searchByValue(payload, function(err, data){
+                    callback(err, data);
+                });
+                break;
+            case 'delete':
+                hdb_delete.delete(payload, function(err, data){
+                    callback(err, data);
+                });
+                break;
+        }
+        return;
+    }
+
+
+
+    function isJson(string){
+        try {
+            JSON.parse(string);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    }
+}
+
 
