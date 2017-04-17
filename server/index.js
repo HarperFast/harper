2995e10dacfe3df89455f839f16a946f0559c0b1
Kyle Bernhardy <kyle@harperdb.io>
2017-04-17 17:07:29 -0600

    added cluster to tcp server

--- server/index.js
+++ server/index.js
@@ -5,18 +5,13 @@
         search  = require('../data_layer/search.js'),
         hdb_delete = require('../data_layer/delete.js'),
         max_data_size = 65536,
-        net = require('net');
+        net = require('net'),
+        cluster = require('cluster');
+
     var numPorts = settings.TCP_PORT_RANGE_END - settings.TCP_PORT_RANGE_BEGIN;
     var counter =0;
-    while(counter < numPorts + 1){
-        let port  = settings.TCP_PORT_RANGE_BEGIN + counter;
-        net.createServer(conn).listen(port, settings.HDB_ADDRESS);
-        counter++;
-    }
 
-
-
-function conn(socket){
+function conn(socket) {
     let socket_data = '';
 
     socket.on('error', (err) => {
@@ -29,18 +24,18 @@ function conn(socket){
 
     socket.on('data', onSocketData);
 
-    function onSocketData(data){
+    function onSocketData(data) {
         socket_data += data;
 
-        if(data.length <= max_data_size && isJson(socket_data)) {
+        if (data.length <= max_data_size && isJson(socket_data)) {
             let json = JSON.parse(socket_data);
-            if(!Object.keys(json)[0]){
+            if (!Object.keys(json)[0]) {
                 socket.end('Missing operation');
                 return;
             }
 
-            handleOperation(json, function(err, data){
-                if(err) {
+            handleOperation(json, function (err, data) {
+                if (err) {
                     console.error(err);
                     socket.end(JSON.stringify(err));
                     return;
@@ -52,36 +47,35 @@ function conn(socket){
             });
 
 
-
         }
     }
 
-    function handleOperation(json, callback){
+    function handleOperation(json, callback) {
         let payload = json[Object.keys(json)[0]];
         console.log(payload);
-        switch(Object.keys(json)[0]){
+        switch (Object.keys(json)[0]) {
             case 'write':
-                insert.insert(payload, function(err, data){
+                insert.insert(payload, function (err, data) {
                     callback(err, data);
                 });
                 break;
             case 'search_by_hash':
-                search.searchByHash(payload, function(err, data){
+                search.searchByHash(payload, function (err, data) {
                     callback(err, data);
                 });
                 break;
             case 'search_by_hashes':
-                search.searchByHashes(payload, function(err, data){
+                search.searchByHashes(payload, function (err, data) {
                     callback(err, data);
                 });
                 break;
             case 'search_by_value':
-                search.searchByValue(payload, function(err, data){
+                search.searchByValue(payload, function (err, data) {
                     callback(err, data);
                 });
                 break;
             case 'delete':
-                hdb_delete.delete(payload, function(err, data){
+                hdb_delete.delete(payload, function (err, data) {
                     callback(err, data);
                 });
                 break;
@@ -90,8 +84,7 @@ function conn(socket){
     }
 
 
-
-    function isJson(string){
+    function isJson(string) {
         try {
             JSON.parse(string);
         } catch (e) {
@@ -101,4 +94,15 @@ function conn(socket){
     }
 }
 
+if (cluster.isMaster) {
+    // Fork workers.
+    for (var i = 0; i <= numPorts; i++) {
+        cluster.fork({port:settings.TCP_PORT_RANGE_BEGIN + i});
+    }
+} else {
 
+    let port = process.env['port'];
+    console.log(port);
+    net.createServer(conn).listen(port, settings.HDB_ADDRESS);
+    counter++;
+}
