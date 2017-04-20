ed4bc67ef23b7468e8ebc9a250c1a131402d1f75
Kyle Bernhardy <kyle@harperdb.io>
2017-04-20 16:12:55 -0600

    TCP fixes

--- data_layer/insert.js
+++ data_layer/insert.js
@@ -12,10 +12,10 @@ const insert_validator = require('../validation/insertValidator.js'),
 
 const hdb_path = path.join(settings.HDB_ROOT, '/schema');
 const regex = /[^0-9a-z]/gi;
-const printf_command = 'printf "%s" > %s';
+const printf_command = 'printf "%s" > %s &';
 const mkdir_command = 'mkdir -p %s';
 const cd_command = 'cd %s';
-const insert_script_command = 'dash %s && nohup dash %s >/dev/null 2>&1 & ';
+const insert_script_command = 'time dash %s & nohup dash %s >/dev/null 2>&1 & ';
 const delete_command = 'rm -f %s';
 const symbolic_link_command = 'ln -sfT %s %s';
 const shebang = '#!/usr/bin/env bash';
@@ -40,8 +40,20 @@ module.exports = {
         //TODO verify hash_attribute is correct for this table
 
         //preprocess all record attributes
-        checkAttributeSchema(insert_object, function(error, attributes, links){
-            insertObject(insert_object.schema, insert_object.table, attributes, links, function(err, data){
+        async.waterfall([
+            checkAttributeSchema.bind(null, insert_object),
+            writeScripts.bind(null, insert_object.schema, insert_object.table),
+            executeScripts
+        ], (err) =>{
+            if(err){
+                callback(err);
+                return;
+            }
+
+            callback(null, `successfully wrote ${insert_object.records.length} records`);
+        });
+        /*checkAttributeSchema(insert_object, function(error, attributes, links){
+            insert_object(insert_object.schema, insert_object.table, attributes, links, function(err, data){
                 if(err) {
                     callback(err);
                     return;
@@ -50,7 +62,7 @@ module.exports = {
                 callback(null, `successfully wrote ${insert_object.records.length} records`);
             });
         });
-
+        */
     }
 };
 
@@ -84,21 +96,27 @@ function checkAttributeSchema(insert_object, callerback) {
 
 
                 link_objects.push(util.format(symbolic_link_command, `../__hdb_hash/${attribute_file_name}`, `${attribute_path}/${attribute_file_name}`));
-                touch_links.push(`${attribute_path}/${attribute_file_name}`);
+                //touch_links.push(`${attribute_path}/${attribute_file_name}`);
             } else if(property === insert_object.hash_attribute){
                 hash_folders[attribute_path] = "";
                 attribute_objects.push(util.format(printf_command, JSON.stringify(record).replace(/"/g, '\\\"'), `${attribute_path}/${record[insert_object.hash_attribute]}-${epoch}.hdb`));
             }
         }
         insert_objects.push(attribute_objects.join('\n'));
+        insert_objects.push('wait');
         symbolic_links.push(link_objects.join('\n'));
         callback();
     }, function(err){
-       // insert_objects.unshift(util.format(delete_command, Object.keys(delete_folders).join(" ")));
+        if(err) {
+            callerback(err);
+            return;
+        }
+
+        // insert_objects.unshift(util.format(delete_command, Object.keys(delete_folders).join(" ")));
         insert_objects.unshift(util.format(mkdir_command, Object.keys(hash_folders).join(" ")));
         insert_objects.unshift(util.format(cd_command, base_path));
 
-        symbolic_links.push(touch_links.length > 0 ? util.format(touch_date_command, date, touch_links.join(' ')): "");
+        //symbolic_links.push(touch_links.length > 0 ? util.format(touch_date_command, date, touch_links.join(' ')): "");
 
         symbolic_links.unshift(Object.keys(folders).length > 0 ? util.format(mkdir_command, Object.keys(folders).join(" ")) : "");
         symbolic_links.unshift(util.format(cd_command, base_path));
@@ -114,6 +132,64 @@ function checkPathExists (path) {
     return fs.existsSync(path);
 }
 
+function writeScripts(schema, table, attribute_array, links, callback){
+    let date = new moment();
+    let part_file_name = `${process.pid}-${date.format('HH:mm:ss.' + process.hrtime()[1])}.sh`;
+    let script_path = path.join(settings.HDB_ROOT, `/staging/scripts/${schema}/${table}/${date.format('YYYY-MM-DD')}`);
+    let data_script_path = path.join(script_path, `data-${part_file_name}`);
+    let link_script_path = path.join(script_path, `link-${part_file_name}`);
+    mkdirp(script_path, function (err) {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        async.parallel([
+            writeScript.bind(null, data_script_path, attribute_array),
+            writeScript.bind(null, link_script_path, links)
+        ], function(err, results){
+
+            if(err){
+                callback(err);
+                return;
+            }
+
+            callback(null, results);
+        });
+    });
+}
+
+function writeScript(script_name, data, callback){
+    fs.writeFile(script_name,data.join('\n'), function(err, data){
+        if(err) {
+            callback(err);
+        } else {
+            callback(null, script_name);
+        }
+    });
+}
+
+function executeScripts(files, callback){
+    let terminal = spawn('bash');
+
+    terminal.stdout.on('data', function (data) {
+        console.log('stdout: ' + data);
+    });
+
+    terminal.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+        //callback(data);
+    });
+
+    terminal.on('exit', function (code) {
+        callback(null, null);
+    });
+
+    terminal.stdin.write(util.format(insert_script_command, files[0], files[1]));
+    terminal.stdin.end();
+}
+
+/*
 function insertObject(schema, table, attribute_array, links, callback) {
     //TODO verify that object has hash attribute defined, if not throw error
     let date = new moment();
@@ -161,7 +237,7 @@ function insertObject(schema, table, attribute_array, links, callback) {
 
                 terminal.stderr.on('data', function (data) {
                     console.log('stderr: ' + data);
-                    callback(data);
+                    //callback(data);
                 });
 
                 terminal.on('exit', function (code) {
@@ -175,4 +251,4 @@ function insertObject(schema, table, attribute_array, links, callback) {
         });
     });
 
-}
+}*/
