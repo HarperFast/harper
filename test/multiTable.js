2c1317044d90592317f94fc48d27eddad01537bc
Kyle Bernhardy <kyle@harperdb.io>
2017-05-23 11:18:24 -0600

    added new data loads

--- test/multiTable.js
+++ test/multiTable.js
@@ -5,14 +5,32 @@ const csvFilePath='./data/fci-breeds.csv',
 var insert_object = {
     operation:'insert',
     schema :  'dev',
-    table:'breed',
+    table:'license',
     hash_attribute: 'id',
     records: []
 };
-
+/*
 csv()
     .fromFile(csvFilePath)
-    .on('json',(jsonObj)=>{
+    .on('json',(jsonObj, rowIndex)=>{
+        jsonObj.id = parseInt(rowIndex) +1
+        insert_object.records.push(jsonObj);
+    })
+    .on('done',(error)=>{
+        insert.insert(insert_object, (err, data)=>{
+            if(err){
+                console.error(err);
+            }
+
+            console.log(data);
+        });
+    });*/
+
+csv()
+    .fromFile('./data/2017-dog-license.csv')
+    .on('json',(jsonObj, rowIndex)=>{
+        jsonObj.id = parseInt(rowIndex) + 1;
+        jsonObj.breed = Math.floor(Math.random() * (344 - 0)) + 1;
         insert_object.records.push(jsonObj);
     })
     .on('done',(error)=>{
