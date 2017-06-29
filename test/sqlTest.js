dfc799bf9d3d78bce97c08897b2144c9686da536
Kyle Bernhardy <kyle@harperdb.io>
2017-06-29 11:24:00 -0600

    fixed issue in globalSchema autocast data types in search

--- test/sqlTest.js
+++ test/sqlTest.js
@@ -2,9 +2,9 @@ const sql_trans = require('../sqlTranslator/index');
 
 //let sql = {sql:"INSERT INTO dev.person (id, dog_name) VALUES(1, 'Penny')"};
 
-//let sql = {sql:"select name, section from dev.breed where id = 1"};
+let sql = {sql:"select name, section from dev.breed where id = 1"};
 //let sql = {sql:"update dev.breed set section = 'stuff' where id = 2"};
-let sql = {sql:"DELETE FROM dev.dog WHERE adorable = 'true'"};
+//let sql = {sql:"DELETE FROM dev.dog WHERE adorable = 'true'"};
 console.time('sql');
 sql_trans.evaluateSQL(sql, (err, data) => {
     console.timeEnd('sql');
