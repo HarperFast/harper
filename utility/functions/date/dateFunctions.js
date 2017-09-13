b82d4d3ea017a13bc5e93ddd22e012fd1a925590
Kyle Bernhardy <kyle@harperdb.io>
2017-09-13 10:27:09 -0600

    HDB-184

--- utility/functions/date/dateFunctions.js
+++ utility/functions/date/dateFunctions.js
@@ -11,6 +11,30 @@ module.exports = {
         return moment().format('YYYY-MM-DD HH:mm:ss');
     },
     extract: (date_part, date)=>{
-        let the_date = moment(date);
+        switch(date_part.toLowerCase()){
+            case 'year':
+                return moment(date).format('YYYY');
+                break;
+            case 'month':
+                return moment(date).format('MM');
+                break;
+            case 'day':
+                return moment(date).format('DD');
+                break;
+            case 'hour':
+                return moment(date).format('HH');
+                break;
+            case 'minute':
+                return moment(date).format('mm');
+                break;
+            case 'second':
+                return moment(date).format('ss');
+                break;
+            case 'millisecond':
+                return moment(date).format('SSS');
+                break;
+            default:
+                break;
+        }
     }
 };
