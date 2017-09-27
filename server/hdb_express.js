5e1faa249c84781a74084787a880530c2581fb05
Kyle Bernhardy <kyle@harperdb.io>
2017-09-26 20:14:36 -0600

    HDB-208

--- server/hdb_express.js
+++ server/hdb_express.js
@@ -93,7 +93,7 @@ if (cluster.isMaster && !DEBUG) {
                             winston.error(error);
                             if(typeof error != 'object')
                                 error = {"error": error};
-                            res.status(200).json(error);
+                            res.status(500).json(error);
                             return;
                         }
                         if(typeof data != 'object')
