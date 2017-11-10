c4a2aa660ab2d04580e70e0e7172742ed7bac043
eli <elipalmer@gmail.com>
2017-11-10 11:45:58 -0700

    HDB-247 -

--- utility/install/installer.js
+++ utility/install/installer.js
@@ -1,7 +1,8 @@
 /**
  * INSTALLER.JS
  *
- * This module is used to install HarperDB.
+ * This module is used to install HarperDB.  It is meant to be a self contained module which is why it configures
+ * it's own winston instance.
  */
 
 const prompt = require('prompt'),
@@ -22,9 +23,9 @@ let hdb_boot_properties = null,
 
 module.exports = {
     "install": run_install
-}
+};
 
-var wizard_result;
+let wizard_result;
 
 /**
  * Stars the install process by first checking for an existing installation, then firing the steps to complete the install.
@@ -104,35 +105,29 @@ function checkInstall(callback) {
                             });
                         });
                     }
-                    callback(null, false);
-                    return;
+                    return callback(null, false);
                 });
             } else {
-                callback(null, true);
-                return;
+                return callback(null, true);
             }
         } else {
-            callback(null, false);
-            return;
+            return callback(null, false);
         }
     }
     catch(e){
-        callback(null, true);
-        return;
+        return callback(null, true);
     }
 }
 
 function checkRegister(callback) {
-    if (wizard_result.HDB_REGISTER == 'true') {
+    if (wizard_result.HDB_REGISTER === 'true') {
         register = require('../registrationHandler'),
             register.register(prompt, function (err, result) {
                 if (err) {
-                    callback(err);
-                    return;
+                    return callback(err);
                 }
 
-                callback(null, "Successful installation!");
-                return;
+                return callback(null, "Successful installation!");
             });
     }
 }
@@ -187,7 +182,7 @@ function wizard(callback) {
         //Support the tilde command for HOME.
         if(wizard_result.HDB_ROOT.indexOf('~') > -1) {
             let home = process.env['HOME'];
-            if( home != undefined) {
+            if( home !== undefined) {
                 let replacement = wizard_result.HDB_ROOT.replace('~', process.env['HOME']);
                 if( replacement && replacement.length > 0) {
                     wizard_result.HDB_ROOT = replacement;
@@ -199,8 +194,7 @@ function wizard(callback) {
         }
         winston.info('wizard result : ' + JSON.stringify(wizard_result));
         if (err) {
-            callback(err);
-            return;
+            return callback(err);
         }
         callback(null, wizard_result.HDB_ROOT);
     });
@@ -218,14 +212,13 @@ function createAdminUser(callback) {
     role_ops.addRole(role, function(err, result){
        if(err) {
            winston.error('role failed to create ' + err);
-           callback(err);
-           return;
+           return callback(err);
        }
 
         let admin_user = {};
         admin_user.username = wizard_result.HDB_ADMIN_USERNAME;
         admin_user.password =wizard_result.HDB_ADMIN_PASSWORD;
-        admin_user.role = result.id
+        admin_user.role = result.id;
         admin_user.active = true;
 
         user_ops.addUser(admin_user, function(err, result) {
@@ -233,18 +226,15 @@ function createAdminUser(callback) {
                winston.error('user creation error' + err);
               return callback(err);
            }
-           callback(null);
-           return;
+           return callback(null);
         });
     });
 }
 
-
 function createSettingsFile(mount_status, callback) {
     console.log('Starting HarperDB Install...');
-    if (mount_status != 'complete') {
-        callback('mount failed');
-        return;
+    if (mount_status !== 'complete') {
+        return callback('mount failed');
     }
 
     createBootPropertiesFile(`${wizard_result.HDB_ROOT}/config/settings.js`, (err) => {
@@ -252,21 +242,21 @@ function createSettingsFile(mount_status, callback) {
 
         if (err) {
             winston.info('info', 'boot properties error' + err);
-            callback(err);
-            return;
+            return callback(err);
         }
 
         const path = require('path');
-        let hdb_props_value = `PROJECT_DIR = ${path.resolve(process.cwd(),'../')}
-        HDB_ROOT= ${wizard_result.HDB_ROOT}
-        HTTP_PORT = ${wizard_result.HTTP_PORT}
-        HTTPS_PORT = ${wizard_result.HTTPS_PORT}
-        CERTIFICATE = ${wizard_result.HDB_ROOT}/keys/certificate.pem
-        PRIVATE_KEY = ${wizard_result.HDB_ROOT}/keys/privateKey.pem
-        HTTPS_ON = FALSE
-        HTTP_ON = TRUE
-        CORS_ON=TRUE
-        CORS_WHITELIST=`;
+        let hdb_props_value = `PROJECT_DIR = ${path.resolve(process.cwd(),'../')}\n` +
+            `HDB_ROOT = ${wizard_result.HDB_ROOT}\n` +
+            `HTTP_PORT = ${wizard_result.HTTP_PORT}\n` +
+            `HTTPS_PORT = ${wizard_result.HTTPS_PORT}\n` +
+            `CERTIFICATE = ${wizard_result.HDB_ROOT}/keys/certificate.pem\n` +
+            `PRIVATE_KEY = ${wizard_result.HDB_ROOT}/keys/privateKey.pem\n` +
+            `HTTPS_ON = FALSE\n` +
+            `HTTP_ON = TRUE \n` +
+            `CORS_ON = TRUE\n` +
+            `SERVER_TIMEOUT_MS = 120000\n` +
+            `CORS_WHITELIST =\n`;
 
         winston.info('info', `hdb_props_value ${JSON.stringify(hdb_props_value)}`);
         winston.info('info', `settings path: ${hdb_boot_properties.get('settings_path')}`);
@@ -276,8 +266,7 @@ function createSettingsFile(mount_status, callback) {
                     winston.error(err);
                 }
                 hdb_properties = PropertiesReader(hdb_boot_properties.get('settings_path'));
-                callback(null);
-                return;
+                return callback(null);
             });
         }catch(e)
         {
@@ -378,25 +367,22 @@ function generateKeys(callback) {
 
 function setupService(callback) {
     fs.readFile(`./utility/install/harperdb.service`, 'utf8', function (err, data) {
-        var fileData = data.replace('{{project_dir}}', `${hdb_properties.get('PROJECT_DIR')}`).replace('{{hdb_directory}}',
+        let fileData = data.replace('{{project_dir}}', `${hdb_properties.get('PROJECT_DIR')}`).replace('{{hdb_directory}}',
             hdb_properties.get('HDB_ROOT'));
         fs.writeFile('/etc/systemd/system/harperdb.service', fileData, function (err, result) {
 
             if (err) {
                 winston.info('error', `Service Setup Error ${err}`);
-                callback(err);
-                return;
+                return callback(err);
             }
 
-            var terminal = spawn('bash');
+            let terminal = spawn('bash');
             terminal.stderr.on('data', function (data) {
             });
 
             terminal.stdin.write(`sudo systemctl daemon-reload &`);
             terminal.stdin.end();
-
-            callback(null, 'success');
-            return;
+            return callback(null, 'success');
         });
     });
 }
@@ -405,8 +391,7 @@ function createBootPropertiesFile(settings_path, callback) {
     winston.info('info', 'creating boot file');
     if (!settings_path) {
         winston.info('info', 'missing settings path');
-        callback('missing setings');
-        return;
+        return callback('missing setings');
     }
 
     let boot_props_value = `settings_path = ${settings_path}
@@ -417,13 +402,11 @@ function createBootPropertiesFile(settings_path, callback) {
         if (err) {
             winston.info('info', `Bootloader error ${err}`);
             winston.info(err);
-            callback(err);
-            return;
+            return callback(err);
         }
         winston.info('info', `props path ${process.cwd()}/../hdb_boot_properties.file`)
         hdb_boot_properties = PropertiesReader(`${process.cwd()}/../hdb_boot_properties.file`);
         winston.info('hdb_boot_properties' + hdb_boot_properties);
-        callback(null, 'success');
-        return;
+        return callback(null, 'success');
     });
 }
