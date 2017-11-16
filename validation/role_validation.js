84560c82dfc270d0ee922eb07c52f82f462eb63d
eli <elipalmer@gmail.com>
2017-11-16 09:11:49 -0700

    normalize role validation to use errors

--- validation/role_validation.js
+++ validation/role_validation.js
@@ -56,66 +56,72 @@ function customValidate(object) {
                 for(t in schema.tables){
                     let table = schema.tables[t];
                     if(!validate.isDefined(table.read)){
-                        validationErrors.push(`Missing read permission on ${t}`)
+                        validationErrors.push(new Error(`Missing read permission on ${t}`));
                     }
 
                     if(!validate.isDefined(validate.isBoolean(table.read))){
-                        validationErrors.push(`${t}.read must be a boolean`)
+                        validationErrors.push(new Error(`${t}.read must be a boolean`));
                     }
 
                     if(!validate.isDefined(table.insert)){
-                        validationErrors.push(`Missing insert permission on ${t}`)
+                        validationErrors.push(new Error(`Missing insert permission on ${t}`));
                     }
 
                     if(!validate.isDefined(validate.isBoolean(table.insert))){
-                        validationErrors.push(`${t}.insert must be a boolean`)
+                        validationErrors.push(new Error(`${t}.insert must be a boolean`));
                     }
 
                     if(!validate.isDefined(table.update)){
-                        validationErrors.push(`Missing update permission on ${t}`)
+                        validationErrors.push(new Error(`Missing update permission on ${t}`));
                     }
 
                     if(!validate.isBoolean(table.update)){
-                        validationErrors.push(`${t}.update must be a boolean`)
+                        validationErrors.push(new Error(`${t}.update must be a boolean`));
                     }
 
                     if(!validate.isDefined(table.delete)){
-                        validationErrors.push(`Missing delete permission on ${t}`)
+                        validationErrors.push(new Error(`Missing delete permission on ${t}`));
                     }
 
                     if(!validate.isBoolean(table.delete)){
-                        validationErrors.push(`${t}.delete must be a boolean`)
+                        validationErrors.push(new Error(`${t}.delete must be a boolean`));
                     }
 
                     if(table.attribute_restrictions){
                         for(r in table.attribute_restrictions){
                             let restriction = table.attribute_restrictions[r];
                             if(!validate.isDefined(restriction.attribute_name))
-                                validationErrors.push(`attribute_restriction must have an attribute_name`);
+                                validationErrors.push(new Error(`attribute_restriction must have an attribute_name`));
                             if(!validate.isDefined(restriction.read))
-                                validationErrors.push(`attribute_restriction missing read permission`);
+                                validationErrors.push(new Error(`attribute_restriction missing read permission`));
                             if(!validate.isDefined(restriction.insert))
-                                validationErrors.push(`attribute_restriction missing insert permission`);
+                                validationErrors.push(new Error(`attribute_restriction missing insert permission`));
                             if(!validate.isDefined(restriction.update))
-                                validationErrors.push(`attribute_restriction missing update permission`);
+                                validationErrors.push(new Error(`attribute_restriction missing update permission`));
                             if(!validate.isDefined(restriction.delete))
-                                validationErrors.push(`attribute_restriction missing delete permission`);
+                                validationErrors.push(new Error(`attribute_restriction missing delete permission`));
                             if(!validate.isBoolean(restriction.read))
-                                validationErrors.push('attribute_restriction.read must be boolean');
+                                validationErrors.push(new Error('attribute_restriction.read must be boolean'));
                             if(!validate.isBoolean(restriction.insert))
-                                validationErrors.push('attribute_restriction.insert must be boolean');
+                                validationErrors.push(new Error('attribute_restriction.insert must be boolean'));
                             if(!validate.isBoolean(restriction.update))
-                                validationErrors.push('attribute_restriction.update must be boolean');
+                                validationErrors.push(new Error('attribute_restriction.update must be boolean'));
                             if(!validate.isBoolean(restriction.delete))
-                                validationErrors.push('attribute_restriction.delete must be boolean');
+                                validationErrors.push(new Error('attribute_restriction.delete must be boolean'));
                         }
                     }
                 }
             }
         }
     }
-    if(validationErrors.length > 0)
-        return validationErrors;
+    if(validationErrors.length > 0) {
+        let validation_message = '';
+        validationErrors.forEach( (valError)=> {
+            validation_message += `${valError.message}. `;
+        });
+
+        return new Error(validation_message);
+    }
     return null;
 }
 
