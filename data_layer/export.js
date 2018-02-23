929ea9fc4830c753fffc6e1c9752ac3f1ddd74bc
Stephen Goldberg <stephen@harperdb.io>
2018-02-23 14:24:09 -0700

    HDB-351
    HDB-351

--- data_layer/export.js
+++ data_layer/export.js
@@ -1,51 +1,53 @@
 const search = require('./search'),
-      sql = require('../sqlTranslator/index').evaluateSQL,
-      AWS = require('aws-sdk');
+    sql = require('../sqlTranslator/index').evaluateSQL,
+    AWS = require('aws-sdk'),
+    Json2csvParser = require('json2csv').Parser,
+    fs = require('fs');
 
 
-    module.exports = {
-        export_to_s3: export_to_s3
-    };
+module.exports = {
+    export_to_s3: export_to_s3
+};
 
-function export_to_s3 (export_object, callback){
-    if(!export_object.format ){
+function export_to_s3(export_object, callback) {
+    if (!export_object.format) {
         return callback("format missing");
     }
 
-    if(export_object.format != 'json' && export_object.format != 'csv' ){
+    if (export_object.format != 'json' && export_object.format != 'csv') {
         return callback("format invalid. must be json or csv.");
     }
 
-    if(!export_object.s3){
+    if (!export_object.s3) {
         return callback("S3 object missing");
     }
 
-    if(!export_object.s3.aws_access_key_id){
+    if (!export_object.s3.aws_access_key_id) {
         return callback("S3.aws_access_key_id missing");
     }
 
-    if(!export_object.s3.aws_secret_access_key){
+    if (!export_object.s3.aws_secret_access_key) {
         return callback("S3.aws_secret_access_key missing");
     }
 
-    if(!export_object.s3.bucket){
+    if (!export_object.s3.bucket) {
         return callback("S3.bucket missing");
     }
 
-    if(!export_object.s3.key){
+    if (!export_object.s3.key) {
         return callback("S3.key missing");
     }
 
-    if(!export_object.search_operation){
+    if (!export_object.search_operation) {
         return callback("search_operation missing");
     }
 
-    if(!export_object.search_operation.operation){
+    if (!export_object.search_operation.operation) {
         return callback("search_operation.operation missing");
     }
 
     let operation;
-    switch(export_object.search_operation.operation){
+    switch (export_object.search_operation.operation) {
         case 'search_by_value':
             operation = search.searchByValue;
             break;
@@ -58,30 +60,52 @@ function export_to_s3 (export_object, callback){
     }
 
 
-    operation(export_object.search_operation, function(err, results){
-        if(err){
+    operation(export_object.search_operation, function (err, results) {
+        if (err) {
             return callback(err);
         }
         AWS.config.update({
-            accessKeyId: export_object.s3.aws_access_key_id ,
-            secretAccessKey:export_object.s3.aws_secret_access_key
+            accessKeyId: export_object.s3.aws_access_key_id,
+            secretAccessKey: export_object.s3.aws_secret_access_key
         });
-        var s3 = new AWS.S3();
-        params = {Bucket: export_object.s3.bucket, Key: export_object.s3.key, Body: JSON.stringify(results)};
-        s3.putObject(params, function(err, data) {
 
-            if (err) {
-               return callback(err);
+        let fields = [];
+        let s3_object;
+        if (export_object.format === 'csv') {
+            for (key in results[0]) {
+                fields.push(key);
             }
 
-            return callback(null, data);
-        });
+
+            let  parser = new Json2csvParser({fields});
+            let csv = parser.parse(results);
+            sendToS3(csv, export_object.s3.key + ".csv");
+
+
+        }
+
+        if(export_object.format === 'json'){
+            s3_object = results;
+            sendToS3(JSON.stringify(s3_object), export_object.s3.key + ".json");
+        }
+
+
+        function sendToS3(s3_object, file_name){
+            var s3 = new AWS.S3();
+            params = {Bucket: export_object.s3.bucket, Key: file_name, Body: s3_object};
+            s3.putObject(params, function (err, data) {
+
+                if (err) {
+                    return callback(err);
+                }
+
+                return callback(null, data);
+            });
+        }
 
 
 
     });
-
-
 
 
 }
