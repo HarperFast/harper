a99ce3b607fcbfdf1e836f558d753e3b0cd0b2f0
zaxharperdb <zachary@harperdb.io>
2018-04-17 20:19:54 +0000

    update postman collection to run against added new subdirectories in utility/functions

--- utility/devops/docker_trusty_local.sh
+++ utility/devops/docker_trusty_local.sh
@@ -18,7 +18,7 @@ sed -i "/HDB_PROC_NAME/ s/ =.*/ = 'no_oneis_here';/" /opt/harperdb/bin/run.js
 #"data_layer" "sqlTranslator" "validation" "security" "utility" "utility/install" "utility/logging" 
 #"utility/functions/date" "utility/functions/math" "lib/fileSystem" "lib/server" "lib/streams" "server" "server/clustering"
 
-files=( "data_layer" "sqlTranslator" "validation" "security" "utility" "utility/install" "utility/logging" "utility/functions/date" "utility/functions/math" "lib/fileSystem" "lib/server" "lib/streams" "server" "server/clustering" "json" )
+files=( "data_layer" "sqlTranslator" "validation" "security" "utility" "utility/install" "utility/logging" "utility/functions/date" "utility/functions/math" "utility/functions/string" "utility/functions/sql" "lib/fileSystem" "lib/server" "lib/streams" "server" "server/clustering" "json" )
 working_dir="$(pwd)/../../";
 mirrored_dir="/tmp/harperdb_dev"
 echo "here i am $working_dir"
