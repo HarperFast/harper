fdd54f8403523ca83d22fdd3d028ef1720ee8400
Kyle Bernhardy <kyle@harperdb.io>
2018-04-30 14:24:39 -0600

    HDB-420
    Breaking out code to be reused for new function

--- data_layer/export.js
+++ data_layer/export.js
@@ -4,6 +4,10 @@ const search = require('./search');
 const sql = require('../sqlTranslator/index').evaluateSQL;
 const AWS = require('aws-sdk');
 const Json2csvParser = require('json2csv').Parser;
+const hdb_utils = require('../utility/common_utils');
+
+const VALID_SEARCH_OPERATIONS = ['search_by_value', 'search_by_hash', 'sql'];
+const VALID_EXPORT_FORMATS = ['json', 'csv'];
 
 module.exports = {
     export_to_s3: export_to_s3,
@@ -20,57 +24,61 @@ function export_to_local(export_object, callback) {
 }
 
 function export_to_s3(export_object, callback) {
-    if (!export_object.format) {
-        return callback("format missing");
+    let error_message = exportCoreValidation(export_object);
+
+    if(!hdb_utils.isEmpty(error_message)){
+        return callback(error_message);
     }
 
-    if (export_object.format !== 'json' && export_object.format !== 'csv') {
-        return callback("format invalid. must be json or csv.");
-    }
-
-    if (!export_object.s3) {
+    if (hdb_utils.isEmpty(export_object.s3)) {
         return callback("S3 object missing");
     }
 
-    if (!export_object.s3.aws_access_key_id) {
+    if (hdb_utils.isEmpty(export_object.s3.aws_access_key_id)) {
         return callback("S3.aws_access_key_id missing");
     }
 
-    if (!export_object.s3.aws_secret_access_key) {
+    if (hdb_utils.isEmpty(export_object.s3.aws_secret_access_key)) {
         return callback("S3.aws_secret_access_key missing");
     }
 
-    if (!export_object.s3.bucket) {
+    if (hdb_utils.isEmpty(export_object.s3.bucket)) {
         return callback("S3.bucket missing");
     }
 
-    if (!export_object.s3.key) {
+    if (hdb_utils.isEmpty(export_object.s3.key)) {
         return callback("S3.key missing");
     }
 
-    if (!export_object.search_operation) {
-        return callback("search_operation missing");
-    }
+    searchAndConvert(export_object, function handleResults(err, data){
+        AWS.config.update({
+            accessKeyId: export_object.s3.aws_access_key_id,
+            secretAccessKey: export_object.s3.aws_secret_access_key
+        });
 
-    if (!export_object.search_operation.operation) {
-        return callback("search_operation.operation missing");
-    }
+        let s3_data;
+        let s3_name;
+        if(export_object.format === 'csv'){
+            s3_data = data;
+            s3_name = export_object.s3.key + ".csv";
+        } else if(export_object.format === 'json'){
+            s3_data = JSON.stringify(data);
+            s3_name = export_object.s3.key + ".json";
+        } else {
+            return callback("an unexpected exception has occurred, please check your request and try again.");
+        }
 
-    let operation;
-    switch (export_object.search_operation.operation) {
-        case 'search_by_value':
-            operation = search.searchByValue;
-            break;
-        case 'search_by_hash':
-            operation = search.searchByHash;
-            break;
-        case 'sql':
-            operation = sql;
-            break;
-    }
+        var s3 = new AWS.S3();
+        let params = {Bucket: export_object.s3.bucket, Key: s3_name, Body: s3_data};
+        s3.putObject(params, function (err, data) {
+            if (err) {
+                return callback(err);
+            }
 
-//in order to validate the search function and invoke permissions we need to add the hdb_user to the search_operation
-    export_object.search_operation.hdb_user = export_object.hdb_user;
+            return callback(null, data);
+        });
+
+    });
 
     operation(export_object.search_operation, function (err, results) {
         if (err) {
@@ -92,8 +100,6 @@ function export_to_s3(export_object, callback) {
             let  parser = new Json2csvParser({fields});
             let csv = parser.parse(results);
             sendToS3(csv, export_object.s3.key + ".csv");
-
-
         }
 
         if(export_object.format === 'json'){
@@ -102,22 +108,69 @@ function export_to_s3(export_object, callback) {
         }
 
 
-        function sendToS3(s3_object, file_name){
-            var s3 = new AWS.S3();
-            params = {Bucket: export_object.s3.bucket, Key: file_name, Body: s3_object};
-            s3.putObject(params, function (err, data) {
 
-                if (err) {
-                    return callback(err);
-                }
-
-                return callback(null, data);
-            });
-        }
 
 
 
     });
 
 
+}
+
+
+
+function exportCoreValidation(export_object){
+    if (hdb_utils.isEmpty(export_object.format)) {
+        return "format missing";
+    }
+
+    if (VALID_EXPORT_FORMATS.indexOf(export_object.format) < 0) {
+        return `format invalid. must be one of the following values: ${VALID_EXPORT_FORMATS.join(', ')}`;
+    }
+
+    let search_operation = export_object.search_operation.operation;
+    if (hdb_utils.isEmpty(search_operation)) {
+        return "search_operation.operation missing";
+    }
+
+    if(VALID_SEARCH_OPERATIONS.indexOf(search_operation) < 0 ){
+        return `search_operation.operation must be one of the following values: ${VALID_SEARCH_OPERATIONS.join(', ')}`
+    }
+}
+
+function searchAndConvert(export_object, callback){
+    let operation;
+    switch (export_object.search_operation.operation) {
+        case 'search_by_value':
+            operation = search.searchByValue;
+            break;
+        case 'search_by_hash':
+            operation = search.searchByHash;
+            break;
+        case 'sql':
+            operation = sql;
+            break;
+    }
+
+    //in order to validate the search function and invoke permissions we need to add the hdb_user to the search_operation
+    export_object.search_operation.hdb_user = export_object.hdb_user;
+
+    operation(export_object.search_operation, function (err, results) {
+        if (err) {
+            return callback(err);
+        }
+
+        if(export_object.format === 'json'){
+            return callback(null, results);
+        } else if (export_object.format === 'csv') {
+            let fields = [];
+            for (let key in results[0]) {
+                fields.push(key);
+            }
+
+            let  parser = new Json2csvParser({fields});
+            let csv = parser.parse(results);
+            return callback(null, csv);
+        }
+    });
 }
