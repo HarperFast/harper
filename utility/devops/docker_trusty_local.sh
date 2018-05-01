9badab1d1a81dac14caf6cc1455b318f379d2c57
Zachary Fowler <zachary@harperdb.io>
2018-05-01 15:39:22 -0600

    Update docker_trusty_local.sh

--- utility/devops/docker_trusty_local.sh
+++ utility/devops/docker_trusty_local.sh
@@ -67,20 +67,14 @@ harperdb_run()
 	theProc=$(ps -ef | grep [h]db_express);
 
 	if [ "$theProc" ]; then
-		apiKey='fe1dfb2c3647474f8f3e9d836783e694'
-		# mycos Collection
-		collection_id='45f26d10-5af1-3f5d-b00b-a39a52c9aa45'
+		 apiKey=fe1dfb2c3647474f8f3e9d836783e694
 
-		# zach 's dummy tests
-		#collection_id=b21ee620-6c69-7566-9a11-e2ce6ece23cd
-
-		#mycos Environment
-		#environment_id=65398310-b319-fc53-7f6c-78710804cda3
-	
-		#LOCAL EnV
-		environment_id='b26572ae-9cf0-bb2e-565f-3f5c6e30ad07'
-		#zach' s dummmy environment
-		#environment_id=d4f6eefe-b922-9888-043f-43a374a1ef1a
+               #ELI COLLECTION CI
+               collection_id=f3d05f41-9265-42b3-9799-91eff759a589
+               #eli Collection ENV ARM
+               # environment_id=0e9e6428-313b-403e-acb7-75e9ba1efdfd
+               #eli Collection ENV CI
+            	environment_id=58f8be47-5446-4516-8e72-669fe9a41c24
 
 		newman run https://api.getpostman.com/collections/$collection_id?apikey=$apiKey --environment https://api.getpostman.com/environments/$environment_id?apikey=$apiKey -r teamcity
 							
@@ -140,17 +134,13 @@ theProc=$(ps -ef | grep [h]db_express);
 if [ "$theProc" ];
   then
     apiKey=fe1dfb2c3647474f8f3e9d836783e694
-#mycos Collection
-    collection_id=45f26d10-5af1-3f5d-b00b-a39a52c9aa45
 
-#zach's dummy tests
-#collection_id=b21ee620-6c69-7566-9a11-e2ce6ece23cd
-
-#mycos Environment PI 3 env
-    environment_id=f02e9c0e-4dd5-891c-8c32-9ac1efb022fa
-
-#zach's dummmy environment
-#environment_id=d4f6eefe-b922-9888-043f-43a374a1ef1a
+      #ELI COLLECTION CI
+         collection_id=f3d05f41-9265-42b3-9799-91eff759a589
+         #eli Collection ENV ARM
+         environment_id=0e9e6428-313b-403e-acb7-75e9ba1efdfd
+         #eli Collection ENV CI
+         #environment_id=58f8be47-5446-4516-8e72-669fe9a41c24
     newman run https://api.getpostman.com/collections/$collection_id?apikey=$apiKey --environment https://api.getpostman.com/environments/$environment_id?apikey=$apiKey -r cli > ../newman_output.log
 
 ./armv7-harperdb stop
