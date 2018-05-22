d88c990b9fa5a1ff8be215bbeeafa52b81f8797e
Kyle Bernhardy <kyle@harperdb.io>
2018-05-21 18:06:24 -0600

    HDB-194
    Added turf.js
    Implemented base geo functions

---
+++ utility/functions/geo.js
@@ -0,0 +1,57 @@
+const turf = require('@turf/turf');
+
+module.exports = {
+    geoArea: geoArea,
+    geoLength:geoLength,
+    geoCircle: geoCircle,
+    geoDifference: geoDifference,
+    geoDistance :geoDistance,
+    geoNear:geoNear,
+    geoContains:geoContains,
+    geoEqual:geoEqual,
+    geoCrosses:geoCrosses,
+    geoConvert:geoConvert
+};
+
+function geoArea(geo){
+    return turf.area(geo);
+}
+
+function geoLength(geo, units){
+    return turf.length(geo, {units:units ? units : "kilometers"});
+}
+
+function geoCircle(point, radius, units){
+    return turf.circle(point, radius, {units:units ? units : "kilometers"});
+}
+
+function geoDifference(poly1, poly2){
+    return turf.difference(poly1, poly2);
+}
+
+function geoDistance(point1, point2, units){
+    return turf.distance(point1, point2, {units:units ? units : "kilometers"});
+}
+
+function geoNear(point1, point2, distance, units){
+    let points_distance = distance(point1, point2, units);
+    return points_distance <= distance;
+}
+
+function geoContains(geo1, geo2){
+    return turf.booleanContains(geo1, geo2);
+}
+
+function geoEqual(geo1, geo2){
+    return turf.booleanEqual(geo1, geo2);
+}
+
+function geoCrosses(geo1, geo2){
+    //need to do ! as this checks for non-intersections of geometries
+    return !turf.booleanDisjoint(geo1, geo2);
+}
+
+function geoConvert(points, converter){
+    return turf[converter](points);
+}
+
