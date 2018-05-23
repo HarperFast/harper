94f9fb260bde97f2cb9a5d62338345803f39784c
Eli Palmer <eli@harperdb.io>
2018-05-23 15:24:47 -0600

    HDB-425 - Add job infrastructure

--- utility/hdbTerms.js
+++ utility/hdbTerms.js
@@ -7,6 +7,29 @@
 
 const HDB_PROC_NAME = 'hdb_express.js';
 
+const SYSTEM_SCHEMA_NAME = 'system';
+const ROLE_TABLE_NAME = 'hdb_role';
+const JOB_TABLE_NAME = 'hdb_job';
+const JOB_TYPE_ENUM = {
+    CSV_FILE_UPLOAD: 'CSV_FILE_UPLOAD',
+    EMPTY_TRASH: 'EMPTY_TRASH',
+    CSV_URL_LOAD: 'CSV_URL_LOAD',
+    EXPORT_TO_S3: 'EXPORT_TO_S3',
+    EXPORT_LOCAL: 'EXPORT_LOCAL',
+	TTL: 'TTL'
+};
+const JOB_STATUS_ENUM = {
+	CREATED: "CREATED",
+	IN_PROGRESS: "IN_PROGRESS",
+	COMPLETE: "COMPLETE",
+	ERROR: "ERROR"
+};
+
 module.exports = {
     HDB_PROC_NAME,
+    SYSTEM_SCHEMA_NAME,
+    ROLE_TABLE_NAME,
+    JOB_TABLE_NAME,
+    JOB_TYPE_ENUM,
+    JOB_STATUS_ENUM
 };
