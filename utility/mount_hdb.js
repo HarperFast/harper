39223c140d9224ac5649162db753fdf366742006
Eli Palmer <eli@harperdb.io>
2018-08-10 10:30:01 -0600

    HDB-485 - Restore unneeded changes to mount

--- utility/mount_hdb.js
+++ utility/mount_hdb.js
@@ -4,70 +4,33 @@
  * @Description: Create the filesystem under the path root specified in hdb_path
  */
 
-const fs = require('fs');
-const path = require('path');
-const {promisify} = require('util');
-const log = require('../utility/logging/harper_logger');
-const hdb_util = require('../utility/common_utils');
+const fs = require('fs'),
+    path = require('path');
 
-// Promisified functions
-const p_fs_readdir = promisify(fs.readdir);
+module.exports = function (logger, hdb_path, callback) {
 
-let loaded_directives = [];
+    function makeDirectory(targetDir, {isRelativeToScript = false} = {}) {
+        const sep = path.sep;
+        const initDir = path.isAbsolute(targetDir) ? sep : '';
+        const baseDir = isRelativeToScript ? __dirname : '.';
 
-module.exports = {
-    mountDirectives:mountDirectives
-};
-
-async function readDirectives(hdb_path) {
-    const directive_path = path.join(process.cwd(), 'directives', 'directives');
-    let files = await p_fs_readdir(directive_path);
-    if(!files) {
-        console.error(`No directive files found in path: ${directive_path}`);
-        log.fatal(`No directive files found in path: ${directive_path}`);
-        throw new Error(`No directive files found in path: ${directive_path}`);
-    }
-    for(let i = 0; i<files.length; i++) {
-        let directive = require(`${files[i]}`);
-        loaded_directives.push(directive);
-        log.trace(`loaded directive ${files[i]}`);
-    }
-}
-
-function makeDirectory(targetDir, {isRelativeToScript = false} = {}) {
-    const sep = path.sep;
-    const initDir = path.isAbsolute(targetDir) ? sep : '';
-    const baseDir = isRelativeToScript ? __dirname : '.';
-
-    targetDir.split(sep).reduce((parentDir, childDir) => {
-        const curDir = path.resolve(baseDir, parentDir, childDir);
-        try {
-            if(curDir && curDir !== '/') {
-                fs.mkdirSync(curDir);
-                logger.info(`Directory ${curDir} created`);
+        targetDir.split(sep).reduce((parentDir, childDir) => {
+            const curDir = path.resolve(baseDir, parentDir, childDir);
+            try {
+                if(curDir && curDir !== '/') {
+                    fs.mkdirSync(curDir);
+                    logger.info(`Directory ${curDir} created`);
+                }
+            } catch (err) {
+                if (err.code !== 'EEXIST') {
+                    throw err;
+                }
             }
-        } catch (err) {
-            if (err.code !== 'EEXIST') {
-                throw err;
-            }
-        }
-        return curDir;
-    }, initDir);
-}
-
-async function writeEnvVariables(hdb_path, variable_declarations) {
-
-}
-
-async function mountDirectives(hdb_path, callback) {
-    await readDirectives(hdb_path);
-    if(hdb_util.isEmptyOrZeroLength(loaded_directives)) {
-        console.error(`No install/upgrade directives found.  Exiting.`);
-        log.error(`No install/upgrade directives found.  Exiting.`);
-        process.exit(1);
+            return curDir;
+        }, initDir);
     }
 
-    /*makeDirectory(hdb_path);
+    makeDirectory(hdb_path);
     makeDirectory(path.join(hdb_path, "staging"));
     makeDirectory(path.join(hdb_path, "staging/scripts"));
     makeDirectory(path.join(hdb_path, "staging/symlink_eraser"));
@@ -95,7 +58,6 @@ async function mountDirectives(hdb_path, callback) {
     makeDirectory(path.join(hdb_path, "schema/system/hdb_nodes"));
     makeDirectory(path.join(hdb_path, "schema/system/hdb_job"));
     callback(null, 'complete');
-    */
 };
 
 
