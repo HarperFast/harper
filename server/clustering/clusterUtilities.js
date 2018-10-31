7f9355b021a2bbe70d983b746665e552981e162b
Eli Palmer <eli@harperdb.io>
2018-10-31 12:56:27 -0600

    HDB-585 - Added scan option when new nodes are added

--- server/clustering/clusterUtilities.js
+++ server/clustering/clusterUtilities.js
@@ -3,6 +3,7 @@ const node_Validator = require('../../validation/nodeValidator');
 const hdb_utils = require('../../utility/common_utils');
 const log = require('../../utility/logging/harper_logger');
 
+
 function addNode(new_node, callback){
     // need to clean up new node as it hads operation and user on it
     let validation = node_Validator(new_node);
@@ -23,7 +24,10 @@ function addNode(new_node, callback){
             log.error(`Error adding new cluster node ${new_node_insert}.  ${err}`);
             return callback(err);
         }
-        // re read new node here.
+        // Send IPC message so master will command forks to rescan for new nodes.
+        process.send({
+            "type": "node_added"
+        });
         return callback(null, `successfully added ${new_node.name} to manifest`);
     });
 }
@@ -45,7 +49,62 @@ function payloadHandler(msg){
     }
 };
 
+function clusterMessageHandler(msg) {
+    try {
+        if (msg.type === 'clustering_payload') {
+            global.forkClusterMsgQueue[msg.id] = msg;
+            payloadHandler(msg);
+        } else if (msg.type === 'delegate_thread_response') {
+            global.delegate_callback_queue[msg.id](msg.err, msg.data);
+        }else if (msg.type === 'clustering') {
+            global.clustering_on = true;
+            global.forks.forEach((fork) => {
+                fork.send(msg);
+            });
+        }else if (msg.type === 'schema') {
+            global.forks.forEach((fork) => {
+                fork.send(msg);
+            });
+        }else if (!hdb_utils.isEmptyOrZeroLength(msg.target_process_id)) {
+            // If a process is specified in the message, send this job to that process.
+            let backup_process = undefined;
+            let specified_process = undefined;
+            for (let i = 0; i < global.forks.length; i++) {
+                if (!backup_process && global.forks[i].process.pid !== msg.target_process_id) {
+                    // Set a backup process to send the message to in case we don't find the specified process.
+                    backup_process = global.forks[i];
+                }
+                if (global.forks[i].process.pid === msg.target_process_id) {
+                    specified_process = global.forks[i];
+                    specified_process.send(msg);
+                    log.info(`Processing job on process: ${msg.target_process_id}`);
+                    break;
+                }
+            }
+            if (!specified_process && backup_process) {
+                log.info(`The specified process ${msg.target_process_id} was not found, sending to process ${global.forks[i].pid} instead.`);
+                backup_process.send(msg);
+            }
+
+        } else if (msg.type === 'node_added') {
+            if(hdb_utils.isEmptyOrZeroLength(global.cluster_server)) {
+                log.error('Cluster Server has not been initialized.  Do you have CLUSTERING=true in your config/settings file?');
+                return;
+            }
+            global.cluster_server.scanNodes().then( () => {
+                log.info('Done scanning for new cluster nodes');
+            }).catch( (e) => {
+                log.error('There was an error scanning for new cluster nodes');
+                log.error(e);
+            });
+        }
+    } catch (e) {
+        log.error(e);
+    }
+}
+
 module.exports = {
         addNode: addNode,
-        payloadHandler: payloadHandler
+        payloadHandler: payloadHandler,
+        clusterMessageHandler: clusterMessageHandler
 }
