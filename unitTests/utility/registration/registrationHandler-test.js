277f9e1635566a89b68feb7b2008989cc68afd68
Eli Palmer <eli@harperdb.io>
2018-11-12 12:01:09 -0700

    HDB-614 - Added unit tests for registration handler

---
+++ unitTests/utility/registration/registrationHandler-test.js
@@ -0,0 +1,105 @@
+"use strict"
+/**
+ * Test the registrationHandler module.
+ */
+
+const assert = require('assert');
+const rewire = require('rewire');
+const fs = require('fs-extra');
+const moment = require('moment');
+const sinon = require('sinon');
+const test_utils = require('../../test_utils');
+test_utils.preTestPrep();
+const reg = rewire('../../../utility/registration/registrationHandler');
+const hdb_license = require('../../../utility/registration/hdb_license');
+
+describe(`Test setLicense`, function () {
+    let write_stub = undefined;
+    let sandbox = null;
+    let err = undefined;
+    let setLicense = reg.__get__('setLicense');
+
+    beforeEach(() => {
+        sandbox = sinon.createSandbox();
+    });
+    afterEach(() => {
+        sandbox.restore();
+    });
+    it('Nominal, set license key stub write file', async function () {
+        write_stub = sandbox.stub(fs, 'writeFile').resolves('');
+        let result = undefined;
+        try {
+            result = await setLicense({'key': 'blahblah'});
+        } catch (e) {
+            err = e;
+        }
+        assert.equal(err, undefined, 'expected no exceptions');
+        assert.equal(result, 'Wrote license key file.', 'expected success message');
+    });
+    it('Set license key stub write file, write throws exception', async function () {
+        write_stub = sandbox.stub(fs, 'writeFile').throws(new Error('BAD WRITE'));
+        let result = undefined;
+        try {
+            result = await setLicense({'key': 'blahblah'});
+        } catch (e) {
+            err = e;
+        }
+        assert.equal(result.indexOf('There was an error writing the key') > -1, true, 'expected error message');
+    });
+    it('Set license key invalid json message', async function () {
+        write_stub = sandbox.stub(fs, 'writeFile').throws(new Error('BAD WRITE'));
+        let result = undefined;
+        try {
+            result = await setLicense(null);
+        } catch (e) {
+            err = e;
+        }
+        assert.equal(result, 'Invalid key specified for license file.', 'expected error message');
+    });
+    it('Set license key invalid key in json message', async function () {
+        write_stub = sandbox.stub(fs, 'writeFile').throws(new Error('BAD WRITE'));
+        let result = undefined;
+        try {
+            result = await setLicense({'key':null});
+        } catch (e) {
+            err = e;
+        }
+        assert.equal(result, 'Invalid key specified for license file.', 'expected error message');
+    });
+});
+
+describe(`Test getFingerprint`, function () {
+    let generate_stub = undefined;
+    let sandbox = null;
+    let err = undefined;
+    let getFingerprint = reg.__get__('getFingerprint');
+
+    beforeEach(() => {
+        sandbox = sinon.createSandbox();
+    });
+    afterEach(() => {
+        sandbox.restore();
+    });
+    it('Nominal, set license key stub write file', async function () {
+        generate_stub = sandbox.stub(hdb_license, 'generateFingerPrint').resolves('blahhash');
+        let result = undefined;
+        try {
+            result = await getFingerprint();
+        } catch (e) {
+            err = e;
+        }
+        assert.equal(err, undefined, 'expected no exceptions');
+        assert.equal(result, 'blahhash', 'expected success message');
+    });
+    it('Set license key stub write file, write throws exception', async function () {
+        generate_stub = sandbox.stub(hdb_license, 'generateFingerPrint').resolves('There was an error generating the fingerprint');
+        let result = undefined;
+        try {
+            result = await getFingerprint();
+        } catch (e) {
+            err = e;
+        }
+        assert.equal(result,'There was an error generating the fingerprint', 'expected error message');
+    });
+
+});
