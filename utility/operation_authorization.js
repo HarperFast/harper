ff462cd060ab8e346610bc13036bce5f8086971f
Eli Palmer <eli@harperdb.io>
2018-11-28 12:53:22 -0700

    HDB-588 - Added permissions for configureCluster and removeNode

--- utility/operation_authorization.js
+++ utility/operation_authorization.js
@@ -80,6 +80,8 @@ required_permissions.set(role.dropRole.name, new permission(true, []));
 required_permissions.set(user.userInfo.name, new permission(true, []));
 required_permissions.set(read_log.read_log.name, new permission(true, []));
 required_permissions.set(cluster_utilities.addNode.name, new permission(true, []));
+required_permissions.set(cluster_utilities.removeNode.name, new permission(true, []));
+required_permissions.set(cluster_utilities.configureCluster.name, new permission(true, []));
 required_permissions.set(reg.getFingerprint.name, new permission(true, []));
 required_permissions.set(reg.setLicense.name, new permission(true, []));
 required_permissions.set(data_export.export_to_s3.name, new permission(false, [READ_PERM]));
