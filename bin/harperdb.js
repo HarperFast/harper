7871a79161d3e3a926b6090454be4c626bc62f77
Eli Palmer <eli@harperdb.io>
2018-11-30 14:43:17 -0700

    HDB-631 - registration will print the result properly

--- bin/harperdb.js
+++ bin/harperdb.js
@@ -84,8 +84,10 @@ function harperDBService() {
             case hdb_terms.SERVICE_ACTIONS_ENUM.REGISTER:
                 // register requires a lot of imports that could fail during install, so only bring it in when needed.
                 const register = require('./register');
-                register.register().catch((err) => {
-                    console.error(err.message);
+                register.register().then((response) => {
+                    console.log(response);
+                }).catch((err) => {
+                    console.error(err);
                 });
                 break;
             case hdb_terms.SERVICE_ACTIONS_ENUM.STOP:
