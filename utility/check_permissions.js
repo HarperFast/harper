1cb030448d2d9361bd57b2bd5f05a82df3620cc9
Eli Palmer <eli@harperdb.io>
2019-02-08 11:13:02 -0700

    HDB-709 - Added uid check in install process

--- utility/check_permissions.js
+++ utility/check_permissions.js
@@ -8,7 +8,7 @@ module.exports = {
 function checkPermission () {
     let hdb_boot_properties = PropertiesReader(`${process.cwd()}/../hdb_boot_properties.file`);
 
-    if(os.userInfo().username != hdb_boot_properties.get('install_user')){
+    if(os.userInfo().username !== hdb_boot_properties.get('install_user')){
         throw new Error(`Error: Must execute as ${hdb_boot_properties.get('install_user')}`);
     }
 }
