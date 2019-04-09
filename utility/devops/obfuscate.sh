b1b638a43d4d173ea0a8784423ace090f9a026d5
Ubuntu <ubuntu@ip-172-31-1-93.us-east-2.compute.internal>
2019-04-09 22:18:51 +0000

    fixed order of operations and directory correction

--- utility/devops/obfuscate.sh
+++ utility/devops/obfuscate.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 EXCLUDE="docs,json,integrationTests,node_modules,unitTests,test,utility/devops"
-ADD="node_modules/ integrationTests/ json/ package.json utility/install/ascii_logo.txt utility/install/harperdb.conf utility/install/harperdb.service"
+ADD="node_modules/ integrationTests/ json/ package.json"
+ADD2_DIR="utility/install"
+ADD2="$ADD2_DIR/ascii_logo.txt $ADD2_DIR/harperdb.conf $ADD2_DIR/harperdb.service"
 MIRRORED_DIR="/tmp/harperdb_dev"
 
 #REMOVE FOR PRODUCTION
@@ -8,8 +10,8 @@ sed -i "/HDB_PROC_NAME/ s/ =.*/ = 'no_oneis_here';/" ./bin/run.js
 #NOTE: CREATE JIRA FOR DEV team to remove #!
 sed -i "s/#.*//" ./bin/stop.js
 
-#Copy some required files to the mirrored dir.
-cp -R $ADD $MIRRORED_DIR
-
 javascript-obfuscator ./ --exclude "$EXCLUDE" -c ./utility/devops/obfuscate_config.json -o $MIRRORED_DIR
 
+#Copy some required files to the mirrored dir.
+cp -R $ADD $MIRRORED_DIR
+cp $ADD2 $MIRRORED_DIR/$ADD2_DIR
