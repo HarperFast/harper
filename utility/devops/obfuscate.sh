af5cfacac2049f67fdb881a94bade3085d28e719
Ubuntu <ubuntu@ip-172-31-1-93.us-east-2.compute.internal>
2019-04-10 20:45:19 +0000

    fixed path and copy of devops scripts for testing downstream

--- utility/devops/obfuscate.sh
+++ utility/devops/obfuscate.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-EXCLUDE="docs,json,integrationTests,node_modules,unitTests,test,utility/devops"
+#Excludes from obfuscation
+EXCLUDE="docs,json,integrationTests,node_modules,unitTests,test utility/devops"
+#ADD directories and files required for running and downstream tests
 ADD="node_modules/ integrationTests/ json/ package.json"
+#ADD2_DIR is path for ADD2 misc files for running harperdb.
 ADD2_DIR="utility/install"
 ADD2="$ADD2_DIR/ascii_logo.txt $ADD2_DIR/harperdb.conf $ADD2_DIR/harperdb.service"
+
+#mirrored dir for obfucation output
 MIRRORED_DIR="/tmp/harperdb_dev"
 
 #REMOVE FOR PRODUCTION
@@ -15,3 +20,5 @@ javascript-obfuscator ./ --exclude "$EXCLUDE" -c ./utility/devops/obfuscate_conf
 #Copy some required files to the mirrored dir.
 cp -R $ADD $MIRRORED_DIR
 cp $ADD2 $MIRRORED_DIR/$ADD2_DIR
+mkdir $MIRRORED_DIR/utility/devops
+cp utility/devops/* $MIRRORED_DIR/utility/devops/
