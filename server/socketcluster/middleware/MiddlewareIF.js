d097d1ccdecb2297c0f1c83ec42bccb1d2f6df82
Eli Palmer <eli@harperdb.io>
2019-05-02 14:49:12 -0600

    Added middleware tests

--- server/socketcluster/middleware/MiddlewareIF.js
+++ server/socketcluster/middleware/MiddlewareIF.js
@@ -1,6 +1,7 @@
 "use strict";
 const types = require('../types');
-
+const uuidV4 = require('uuid/v4');
+const log = require('../../../utility/logging/harper_logger');
 /**
  * This is a super class designed to represent a middleware function that is evaluated upon receipt of an inbound message.
  */
@@ -20,12 +21,28 @@ class MiddlewareIF {
     set middleware_type_enum(value) {
         this._middleware_type_enum = value;
     }
+
+    /**
+     * Constructs a middleware object.
+     * @param middleware_type_enum
+     * @param eval_function
+     * @throws
+     */
     constructor(middleware_type_enum, eval_function) {
+        if(middleware_type_enum === undefined || middleware_type_enum === null) {
+            log.warn('Created a MiddlewareIF instance with invalid middleware type.  Defaulting to Subscribe.');
+            middleware_type_enum = types.MIDDLEWARE_TYPE.MIDDLEWARE_SUBSCRIBE;
+        }
+        if(!eval_function) {
+            log.warn('Created a MiddlewareIF instance with invalid middleware type.  Defaulting to empty function.');
+            eval_function = function undef() {return true;};
+        }
         this.middleware_type = middleware_type_enum;
         this.evalFunction = eval_function;
         this.command_order = types.COMMAND_EVAL_ORDER_ENUM.MID;
         this._middleware_type_enum = middleware_type_enum;
         this._eval_function = eval_function;
+        this.id = uuidV4();
     }
 
     /**
@@ -38,7 +55,7 @@ class MiddlewareIF {
 
     setMiddlewareOrder(command_eval_order_enum) {
         log.trace(`setting command order to: ${command_eval_order_enum}`);
-        if(command_eval_order_enum >= 0) {
+        if(command_eval_order_enum > 0 && command_eval_order_enum <= types.COMMAND_EVAL_ORDER_ENUM.VERY_LAST) {
             this.command_order = command_eval_order_enum;
         }
     }
