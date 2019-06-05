678adcae975522d0006fe4aa934a9b4f84fd0f83
Eli Palmer <eli@harperdb.io>
2019-06-05 11:58:46 -0600

    CORE-261 - added smaller prompt library

--- utility/userInterface/upgradePrompt.js
+++ utility/userInterface/upgradePrompt.js
@@ -1,9 +1,13 @@
 "use strict";
 
 const log = require('../logging/harper_logger');
-const inquirer = require('inquirer');
+const {prompt} = require('enquirer');
+const { Select } = require('enquirer');
 const os = require('os');
 const upgrade = require('../../bin/upgrade');
+
+const UPGRADE_PROCEED = 'Yes, proceed';
+const UPGRADE_CANCEL = 'No, cancel the upgrade';
 
 async function forceUpdatePrompt(old_version_string, new_version_string) {
     // pull directive changes
@@ -11,9 +15,13 @@ async function forceUpdatePrompt(old_version_string, new_version_string) {
     let counter = 1;
     let message = 'HarperDB has been recently updated, we need to complete the update process.  If a backup of your data has not been created, cancel this process and backup.  The following data will be affected:';
     message = message + os.EOL;
+    /*
+        Should create a message to the user that describes the changes specified in the directives as a numbered list.
+     */
     changes.forEach((change) => {
        if(change.change_description) {
            message = `${message} ${counter}. ${change.change_description} ${os.EOL}`;
+           counter++;
            if(change.affected_paths.length > 0) {
                change.affected_paths.forEach((path) => {
                    message = `${message} \t - ${path} ${os.EOL}`;
@@ -21,34 +29,26 @@ async function forceUpdatePrompt(old_version_string, new_version_string) {
            }
        }
     });
-    //1.  Inform the user they need to run update, present filesystem changes gathered from upgrade directives
-    let questions = [
-        {
-            type: 'expand',
-            name: 'updateResponse',
-            message: message,
-            choices: [
-                {
-                    key: 'y',
-                    name: 'Run Update',
-                    value: true
-                },
-                {
-                    key: 'n',
-                    name: 'Cancel Update',
-                    value: false
-                }
-            ]
-        }
-    ];
-    //2.  Ask the user to move forward with update, or cancel so they can run a backup.
-    let response = await inquirer.prompt(questions);
-    console.log(`Response is: ${JSON.stringify(response)}`);
-    //3.  Run the upgrade directives
-    if(!response || !response.updateResponse) {
+    let select_questions = new Select({
+       name: 'upgrade',
+       message: message,
+       choices: [
+           UPGRADE_PROCEED,
+           UPGRADE_CANCEL
+       ]
+    });
+
+    //Ask the user to move forward with update, or cancel so they can run a backup.
+    let response = undefined;
+    try {
+        response = await select_questions.run();
+    } catch(err) {
+        log.error('There was an error asking for an upgrade.');
+        log.error(err);
         return false;
     }
-    return true;
+
+    return response === UPGRADE_PROCEED;
 }
 
 module.exports = {
