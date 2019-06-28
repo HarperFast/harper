1263003ac4f9009a3f17858af8dba0ae7ee0ba70
Eli Palmer <eli@harperdb.io>
2019-06-28 12:58:14 -0600

    CORE-353 - added 2.0 and current build directive. Move keys file during upgrade

--- upgrade/directives/2-0-0000.js
+++ upgrade/directives/2-0-0000.js
@@ -9,12 +9,22 @@ const KEYS_FILE_NAME = '060493.ks';
 let home_dir = process.env['HOME'];
 let new_keys_dir_path = `${home_dir}/.harperdb/keys`;
 // Create the ~/.harperdb directory
-directive.relative_directory_paths.push(`${home_dir}/.harperdb`);
+directive.explicit_directory_paths.push(`${home_dir}/.harperdb`);
 // Create the ~/.harperdb/keys directory
-directive.relative_directory_paths.push(new_keys_dir_path);
+directive.explicit_directory_paths.push(new_keys_dir_path);
 
 directive.environment_variables.push(
     new env_variable(`CLUSTERING_USER`, ``, [`The user used to connect to other instances of HarperDB, this user must have a role of cluster_user`])
+);
+
+directive.environment_variables.push(
+    new env_variable(`LOG_DAILY_ROTATE`, `false`, [`Set to true to enable daily log file rotations - each log file name will be prepended with YYYY-MM-DD (for WINSTON logger only)=`])
+);
+
+directive.environment_variables.push(
+    new env_variable(`LOG_MAX_DAILY_FILES`, `false`, [`Set the number of daily log files to maintain when LOG_DAILY_ROTATE is enabled`,
+    'If no integer value is set, no limit will be set for',
+    'daily log files which may consume a large amount of storage depending on your log settings'])
 );
 
 // Move the utilities/keys/060493.ks file to its new home in ~/.harperdb/keys/
@@ -22,10 +32,11 @@ directive.functions.push(() => {
     let old_keys_path = undefined;
     let new_keys_path = undefined;
     try {
-        old_keys_path = path.join(`${process.cwd}`, '../', 'utilities', 'keys', KEYS_FILE_NAME);
+        // Assuming upgrade is being run out of bin/
+        old_keys_path = path.join(`${process.cwd()}`, '../', 'utility', 'keys', KEYS_FILE_NAME);
         console.log(`Checking for ${KEYS_FILE_NAME} file at ${old_keys_path}`);
         if (!fs.existsSync(old_keys_path)) {
-            old_keys_path = path.join(`${process.cwd}`, 'utilities', 'keys', KEYS_FILE_NAME);
+            old_keys_path = path.join(`${process.cwd()}`, '../', 'utility', 'keys', KEYS_FILE_NAME);
             console.log(`${KEYS_FILE_NAME} file not found.  Trying path ${old_keys_path}`);
             if (!fs.existsSync(old_keys_path)) {
                 console.log(`${KEYS_FILE_NAME} file not found.  If you have an enterprise license, please manually move the /utilities/keys/${KEYS_FILE_NAME} into ~/.harperdb/keys/.`);
@@ -37,9 +48,10 @@ directive.functions.push(() => {
             console.log(`Your environment HOME directory is not defined.  If you have an enterprise license, please manually move the /utilities/keys/${KEYS_FILE_NAME} into ~/.harperdb/keys/.`);
         }
         new_keys_path = path.join(new_keys_dir_path, KEYS_FILE_NAME);
-        fs.rename(old_keys_path, new_keys_path);
+        fs.renameSync(old_keys_path, new_keys_path);
     } catch(err) {
         console.error(`There was an error upgrading to version 2.0.  Upgrade will continue, but you may need to manually move your ${old_keys_path} file to ${new_keys_path}`);
+        console.error(err);
     }
 });
 
--- upgrade/directives/directiveManager.js
+++ upgrade/directives/directiveManager.js
@@ -17,6 +17,8 @@ const version_1_2_0_1 = require('./1-2-0-1');
 const version_1_3_0 = require('./1-3-0');
 const version_1_3_001 = require('./1-3-001');
 const version_1_3_1 = require('./1-3-1');
+const version_1_3_1002 = require('./1-3-1002');
+const version_2_0_0000 = require('./2-0-0000');
 
 let versions = new Map();
 
@@ -29,6 +31,8 @@ versions.set(version_1_2_005.version, version_1_2_005);
 versions.set(version_1_3_0.version, version_1_3_0);
 versions.set(version_1_3_001.version, version_1_3_001);
 versions.set(version_1_3_1.version, version_1_3_1);
+versions.set(version_1_3_1002.version, version_1_3_1002);
+versions.set(version_2_0_0000.version, version_2_0_0000);
 
 function getSortedVersions() {
     let sorted_keys = [...versions.keys()].sort(hdb_utils.compareVersions);
