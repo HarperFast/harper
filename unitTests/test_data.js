7a5d991ff389c68f6f43b8fa9a15ba6d34437bad
samj <sam@harperdb.io>
2019-07-09 13:34:19 -0600

    CORE-312 Adds start of test_data file for unit tests

---
+++ unitTests/test_data.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const TEST_DATA_DOG = [
+    {
+        "age": 5,
+        "breed": "Mutt",
+        "id": 1,
+        "name": "Sam"
+    },
+    {
+        "age": 4,
+        "breed": "Golden Retriever",
+        "id": 2,
+        "name": "David"
+    },
+    {
+        "age": 10,
+        "breed": "Pit Bull",
+        "id": 3,
+        "name": "Kyle"
+    },
+    {
+        "age": 10,
+        "breed": "Pit",
+        "id": 4,
+        "name": "Sam"
+    },
+    {
+        "age": 15,
+        "breed": "Poodle",
+        "id": 5,
+        "name": "Eli"
+    },
+    {
+        "age": 8,
+        "breed": "Poodle",
+        "id": 6,
+        "name": "Sarah"
+    }
+];
+
+const TEST_DATA_CAT = [
+    {
+        "age": 5,
+        "id": 1,
+        "name": "Sam"
+    },
+    {
+        "age": 4,
+        "id": 2,
+        "name": "David"
+    }
+];
+
+module.exports = {
+    TEST_DATA_CAT,
+    TEST_DATA_DOG
+};
