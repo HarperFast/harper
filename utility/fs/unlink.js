dbef2586e1b409c5c2e02c559852943157a6bbf7
Kyle Bernhardy <kyle@harperdb.io>
2019-08-01 10:00:15 -0600

    updated unlink to attempt to delete the folder the file being unlinked residesd in.  implemented the function into delete

--- utility/fs/unlink.js
+++ utility/fs/unlink.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const fs_unlink = require('fs-extra').unlink;
+const fs_rmdir = require('fs-extra').rmdir;
 const logger = require('../logging/harper_logger');
+const path = require('path');
 
 module.exports = unlink;
 
@@ -12,13 +14,26 @@ module.exports = unlink;
  */
 async function unlink(paths) {
     await Promise.all(
-        paths.map(async path => {
+        paths.map(async file_path => {
             try {
-                await fs_unlink(path);
+                await fs_unlink(file_path);
             } catch(e){
                 if(e.code !== 'ENOENT'){
                     logger.error(e);
                 }
+            }
+
+            try {
+                //attempt to remove the folder that contains the file
+                let folder = path.dirname(file_path);
+                if(folder.indexOf('__hdb_hash') < 0) {
+                    await fs_rmdir(folder);
+                }
+            }catch(e){
+                if(e.code !== 'ENOTEMPTY'){
+                    logger.error(e);
+                }
+                console.log(e);
             }
         })
     );
