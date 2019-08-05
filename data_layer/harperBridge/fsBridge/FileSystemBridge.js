5894b6f9336eec63fe8b78fc36a7b54d85d3d88f
DavidCockerill <david@harperdb.io>
2019-08-05 10:17:53 -0600

    CORE-442 Debug issues with circular dependencies and the importing of insert.insert

--- data_layer/harperBridge/fsBridge/FileSystemBridge.js
+++ data_layer/harperBridge/fsBridge/FileSystemBridge.js
@@ -1,18 +1,21 @@
 "use strict";
 
 const BridgeMethods = require("../BridgeMethods.js");
-//const fs_create_records = require('./fsMethods/fsCreateRecords');
+
 
 class FileSystemBridge extends BridgeMethods {
 
     //add bridge methods here
 
     async createRecords(insert_obj, attributes, schema_table) {
-        const fs_create_records = require('./fsMethods/fsCreateRecords'); // TODO: only works when this is here
+        //const fs_create_records = require('./fsMethods/fsCreateRecords');
         let result = await fs_create_records(insert_obj, attributes, schema_table);
         return result;
     }
 
 }
 
+const fs_create_records = require('./fsMethods/fsCreateRecords');
 module.exports = FileSystemBridge;
+
+
