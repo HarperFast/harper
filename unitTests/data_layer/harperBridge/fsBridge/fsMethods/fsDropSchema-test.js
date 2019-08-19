ef9b39cf01d04fd7c9cdd77107d92e57466bb992
DavidCockerill <david@harperdb.io>
2019-08-19 10:36:00 -0600

    CORE-438 Start fixing and writing unit tests

--- unitTests/data_layer/harperBridge/fsBridge/fsMethods/fsDropSchema-test.js
+++ unitTests/data_layer/harperBridge/fsBridge/fsMethods/fsDropSchema-test.js
@@ -4,10 +4,11 @@ const test_utils = require('../../../../test_utils');
 test_utils.preTestPrep();
 
 const rewire = require('rewire');
-let fsDropSchema = require('../../../../../data_layer/harperBridge/fsBridge/fsMethods/fsDropSchema');
+let fsDropSchema = rewire('../../../../../data_layer/harperBridge/fsBridge/fsMethods/fsDropSchema');
 let fsDeleteRecords = rewire('../../../../../data_layer/harperBridge/fsBridge/fsMethods/fsDeleteRecords');
 let search = rewire('../../../../../data_layer/search');
 const log = require('../../../../../utility/logging/harper_logger');
+const terms = require('../../../../../utility/hdbTerms');
 const fs = require('graceful-fs');
 const chai = require('chai');
 const sinon = require('sinon');
@@ -38,12 +39,38 @@ const DROP_SCHEMA_OBJ_TEST = {
     schema: "dropTest",
 };
 
-
 describe('Tests for file system module fsDropSchema', () => {
-
+    let sandbox = sinon.createSandbox();
 
     context('Tests for dropSchema function', () => {
+        let fs_delete_records_stub;
+        let p_search_by_value_stub;
+        let move_schema_to_trash_stub;
+        let delete_attr_struc;
+        let schema = DROP_SCHEMA_OBJ_TEST.schema;
+        let delete_schema_obj = {
+            table: terms.SYSTEM_TABLE_NAMES.SCHEMA_TABLE_NAME,
+            schema: terms.SYSTEM_SCHEMA_NAME,
+            hash_values: [schema]
+        };
+        let search_obj = {
+            schema: terms.SYSTEM_SCHEMA_NAME,
+            table: terms.SYSTEM_TABLE_NAMES.TABLE_TABLE_NAME,
+            hash_attribute: terms.SYSTEM_TABLE_HASH,
+            search_attribute: terms.SYSTEM_DEFAULT_ATTRIBUTE_NAMES.ATTR_SCHEMA_KEY,
+            search_value: schema,
+            get_attributes: ['id']
+        };
+        
+        before(() => {
+            fs_delete_records_stub = sandbox.stub();
+            fsDropSchema.__set__();
 
+        });
+
+        after(function () {
+            sandbox.restore('fsDeleteRecords', );
+        });
     });
 
     context('Tests for moveSchemaToTrash function', () => {
