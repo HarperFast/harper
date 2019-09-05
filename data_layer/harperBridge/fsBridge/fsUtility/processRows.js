d018af6364fdc23952ab69bf432fa33725e3f060
DavidCockerill <david@harperdb.io>
2019-09-05 10:49:08 -0600

    CORE-443 update hdb path function in processRows

--- data_layer/harperBridge/fsBridge/fsUtility/processRows.js
+++ data_layer/harperBridge/fsBridge/fsUtility/processRows.js
@@ -5,7 +5,11 @@ const dataWriteProcessor = require('../../../dataWriteProcessor');
 const env = require('../../../../utility/environment/environmentManager');
 const hdb_terms = require('../../../../utility/hdbTerms');
 
-const HDB_PATH = `${env.getHdbBasePath()}/${hdb_terms.HDB_SCHEMA_DIR}/`;
+// Search is used in the installer, and the base path may be undefined when search is instantiated.  Dynamically
+// get the base path from the environment manager before using it.
+let hdb_path = function() {
+    return `${env.getHdbBasePath()}/${hdb_terms.HDB_SCHEMA_DIR}/`;
+};
 
 module.exports = processRows;
 
@@ -21,7 +25,7 @@ async function processRows(insert_obj, attributes, schema_table, existing_rows){
     let epoch = Date.now();
 
     try {
-        let exploder_object = new WriteProcessorObject(HDB_PATH, insert_obj.operation, insert_obj.records, schema_table, attributes, epoch, existing_rows);
+        let exploder_object = new WriteProcessorObject(hdb_path(), insert_obj.operation, insert_obj.records, schema_table, attributes, epoch, existing_rows);
         let data_wrapper = await dataWriteProcessor(exploder_object);
 
         return data_wrapper;
