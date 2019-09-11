b2382b8a862ab9c48752d9161396acaef5d7287f
samj <sam@harperdb.io>
2019-09-11 14:13:02 -0600

    CORE-535 Cleanup before PR

--- data_layer/harperBridge/heBridge/heUtility/heGetAttributeValues.js
+++ data_layer/harperBridge/heBridge/heUtility/heGetAttributeValues.js
@@ -18,31 +18,3 @@ function heGetAttributeValues(hash_values, data_stores) {
         throw err;
     }
 }
-
-// async function readAttributeFilePromise(table_path, attribute, file, attribute_data) {
-//     try {
-//         const data = await fs.readFile(`${table_path}/${hdb_terms.HASH_FOLDER_NAME}/${attribute}/${file}${hdb_terms.HDB_FILE_SUFFIX}`, 'utf-8');
-//         const value = common_utils.autoCast(data.toString());
-//         attribute_data[file] = value;
-//     } catch (err) {
-//         if (err.code !== 'ENOENT') {
-//             throw(err);
-//         }
-//     }
-// }
-//
-// async function readAttributeValues(table_path, attribute, hash_files) {
-//     try {
-//         let attribute_data = {};
-//
-//         const readFileOps = [];
-//         for (const file of hash_files) {
-//             readFileOps.push(readAttributeFilePromise(table_path, attribute, file, attribute_data));
-//         }
-//
-//         await Promise.all(readFileOps);
-//         return attribute_data;
-//     } catch(err) {
-//         throw err;
-//     }
-// }
