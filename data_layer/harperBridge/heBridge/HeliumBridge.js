34cb54f6b0835d0e2474dc14147225f5d5c2c06b
DavidCockerill <david@harperdb.io>
2019-09-17 16:41:31 -0600

    Fix broken unit tests

--- data_layer/harperBridge/heBridge/HeliumBridge.js
+++ data_layer/harperBridge/heBridge/HeliumBridge.js
@@ -16,8 +16,6 @@ const heSearchByHash = require('./heMethods/heSearchByHash');
 // const heCreateTable = require('./heMethods/heCreateTable');
 // const heUpdateRecords = require('./heMethods/heUpdateRecords');
 
-
-
 class HeliumBridge extends BridgeMethods {
 
     async getDataByHash(search_object) {
