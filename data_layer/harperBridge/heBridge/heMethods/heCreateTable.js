6bca78edca991054d2049028d1f606ee982cea71
DavidCockerill <david@harperdb.io>
2019-09-19 12:32:33 -0600

    start building create table

--- data_layer/harperBridge/heBridge/heMethods/heCreateTable.js
+++ data_layer/harperBridge/heBridge/heMethods/heCreateTable.js
@@ -1,18 +1,22 @@
 'use strict';
 
+const hdb_terms = require('../../../../utility/hdbTerms');
+const heCreateRecords = require('../heMethods/heCreateRecords');
+
 module.exports = heCreateTable;
 
-function heCreateTable(table, table_create_obj) {
+function heCreateTable(table_system_data, table_create_obj) {
     let insert_object = {
-        operation: terms.OPERATIONS_ENUM.INSERT,
-        schema: terms.SYSTEM_SCHEMA_NAME,
-        table: terms.SYSTEM_TABLE_NAMES.TABLE_TABLE_NAME,
-        hash_attribute: terms.SYSTEM_TABLE_HASH,
+        operation: hdb_terms.OPERATIONS_ENUM.INSERT,
+        schema: hdb_terms.SYSTEM_SCHEMA_NAME,
+        table: hdb_terms.SYSTEM_TABLE_NAMES.TABLE_TABLE_NAME,
+        hash_attribute: hdb_terms.SYSTEM_TABLE_HASH,
         records: [table_system_data]
     };
 
+    try {
+        heCreateRecords(insert_object);
+    } catch(err) {
 
-
-
-
+    }
 }
