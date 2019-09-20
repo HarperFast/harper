a546c1295971092f6625639dcfeac4dcb3c10d39
DavidCockerill <david@harperdb.io>
2019-09-20 09:29:00 -0600

    CORE-549 Initial commit

--- data_layer/harperBridge/heBridge/HeliumBridge.js
+++ data_layer/harperBridge/heBridge/HeliumBridge.js
@@ -13,7 +13,7 @@ const heSearchByHash = require('./heMethods/heSearchByHash');
 // const heSearchByConditions = require('./heMethods/heSearchByConditions');
 // const heDropSchema = require('./heMethods/heDropSchema');
 const heCreateTable = require('./heMethods/heCreateTable');
-// const heUpdateRecords = require('./heMethods/heUpdateRecords');
+const heUpdateRecords = require('./heMethods/heUpdateRecords');
 
 class HeliumBridge extends BridgeMethods {
     async createSchema(create_schema_obj) {
@@ -37,6 +37,15 @@ class HeliumBridge extends BridgeMethods {
     async createRecords(insert_obj) {
         try {
             return heCreateRecords(insert_obj);
+        } catch(err) {
+            log.error(err);
+            throw err;
+        }
+    }
+
+    async updateRecords(update_obj) {
+        try {
+            return heUpdateRecords(update_obj);
         } catch(err) {
             log.error(err);
             throw err;
---
+++ data_layer/harperBridge/heBridge/heMethods/heUpdateRecords.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = heUpdateRecords;
+
+function heUpdateRecords(update_obj) {
+
+}
