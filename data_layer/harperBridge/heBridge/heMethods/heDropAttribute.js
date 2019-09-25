073f70c4e41534b38a18155b16c56c8688e95a3d
DavidCockerill <david@harperdb.io>
2019-09-25 09:11:54 -0600

    CORE-552 Unit tests for drop attribute

--- data_layer/harperBridge/heBridge/heMethods/heDropAttribute.js
+++ data_layer/harperBridge/heBridge/heMethods/heDropAttribute.js
@@ -1,7 +1,82 @@
 'use strict';
 
+const heGenerateDataStoreName = require('../heUtility/heGenerateDataStoreName');
+const heDeleteRecords = require('./heDeleteRecords');
+const heSearchByValue = require('./heSearchByValue');
+const helium_utils = require('../../../../utility/helium/heliumUtils');
+const hdb_terms = require('../../../../utility/hdbTerms');
+
+let hdb_helium;
+try {
+    hdb_helium = helium_utils.initializeHelium();
+} catch(err) {
+    throw err;
+}
+
 module.exports = heDropAttribute;
 
-function heDropAttribute(drop_attribute_obj) {
+const DROP_ATTR_OBJ_TEST = {
+    operation: "drop_attribute",
+    schema: "dev",
+    table: "dog",
+    attribute: "another_attribute"
+};
 
+const INSERT_OBJ_TEST = {
+    operation: "insert",
+    schema: "system",
+    table: "hdb_attribute",
+    hash_attribute: "id",
+    records: [
+        {
+            "schema": "I am a test",
+            "table": "Not really a table",
+            "id": 45
+        }
+    ]
+};
+
+function heDropAttribute(drop_attribute_obj) {
+    let datastore = [heGenerateDataStoreName(drop_attribute_obj.schema, drop_attribute_obj.table)];
+
+    try {
+        let he_response = hdb_helium.deleteDataStores(datastore);
+        if (he_response[0][1] !== hdb_terms.HELIUM_RESPONSE_CODES.HE_ERR_OK) {
+            throw new Error(he_response[0][1]);
+        }
+
+        return dropAttributeFromSystem(drop_attribute_obj);
+    } catch(err) {
+        throw err;
+    }
+
+}
+
+function dropAttributeFromSystem(drop_attribute_obj) {
+    let search_obj = {
+        schema: 'system',
+        table: 'hdb_attribute',
+        hash_attribute: 'id',
+        search_attribute: 'attribute',
+        search_value: drop_attribute_obj.attribute,
+        get_attributes: ['id']
+    };
+
+    try {
+        let attributes = heSearchByValue(search_obj);
+        if (!attributes || attributes.length < 1) {
+            throw new Error(`Attribute ${drop_attribute_obj.attribute} was not found.`);
+        }
+
+        let delete_table_obj = {
+            table: "hdb_attribute",
+            schema: "system",
+            hash_attribute: "id",
+            hash_values: [attributes[0].id]
+        };
+
+        return heDeleteRecords(delete_table_obj);
+    } catch(err) {
+        throw err;
+    }
 }
--- unitTests/data_layer/harperBridge/heBridge/heMethods/heDropAttribute-test.js
+++ unitTests/data_layer/harperBridge/heBridge/heMethods/heDropAttribute-test.js
@@ -1,1 +1,113 @@
 'use strict';
+
+const test_utils = require('../../../../test_utils');
+test_utils.preTestPrep();
+test_utils.buildHeliumTestVolume();
+
+const heCreateAttribute = require('../../../../../data_layer/harperBridge/heBridge/heMethods/heCreateAttribute');
+const heDropAttribute = require('../../../../../data_layer/harperBridge/heBridge/heMethods/heDropAttribute');
+const heGenerateDataStoreName = require('../../../../../data_layer/harperBridge/heBridge/heUtility/heGenerateDataStoreName');
+const heliumUtils = require('../../../../../utility/helium/heliumUtils');
+const chai = require('chai');
+const { expect } = chai;
+
+let hdb_helium;
+try {
+    heliumUtils.createSystemDataStores();
+    hdb_helium = heliumUtils.initializeHelium();
+} catch(err) {
+    console.log(err);
+}
+
+const DROP_ATTR_OBJ_TEST = {
+    operation: "drop_attribute",
+    schema: "dropAttr",
+    table: "dog",
+    attribute: "another_attribute"
+};
+
+// const CREATE_ATTR_OBJ_TEST = {
+//     operation: "create_attribute",
+//     schema: "attrUnitTest",
+//     table: "dog",
+//     attribute: "",
+// };
+
+const ATTRIBUTES = ['age', 'height', 'weight'];
+const DATASTORES = ['dropAttr/dog/age', 'dropAttr/dog/height', 'dropAttr/dog/weight'];
+
+function setupTest() {
+    try {
+        ATTRIBUTES.forEach((attr) => {
+            let create_attr = {
+                operation: "create_attribute",
+                schema: "dropAttr",
+                table: "dog",
+                attribute: attr,
+            };
+            heCreateAttribute(create_attr);
+        });
+
+        // setTimeout(() => {hdb_helium.createDataStores(DATASTORES);}, 1000);
+        hdb_helium.createDataStores(DATASTORES);
+    } catch(err) {
+        throw err;
+    }
+}
+
+describe('Tests for Helium method heDropAttribute', () => {
+
+    before(() => {
+        global.hdb_schema = {
+            [DROP_ATTR_OBJ_TEST.schema]: {
+                [DROP_ATTR_OBJ_TEST.table]: {
+                    attributes: [{attribute: 'test'}]
+                }
+            },
+            system: {
+                hdb_attribute: {
+                    hash_attribute:"id",
+                    name:"hdb_attribute",
+                    schema:"system",
+                    residence:["*"],
+                    attributes: [
+                        {
+                            attribute: "id"
+                        },
+                        {
+                            attribute: "schema"
+                        },
+                        {
+                            attribute: "table"
+                        },
+                        {
+                            attribute: "attribute"
+                        },
+                        {
+                            attribute: "schema_table"
+                        }
+                    ]
+                }
+            }
+        };
+        setupTest();
+    });
+
+    after(() => {
+        test_utils.teardownHeliumTestVolume(global.hdb_helium);
+    });
+
+
+    context('Test heDropAttribute function', () => {
+        it('ds', () => {
+            console.log('Test');
+            // try {
+            //     console.log(heDropAttribute(DROP_ATTR_OBJ_TEST));
+            // } catch(err) {
+            //     console.log(err);
+            // }
+        });
+
+    });
+
+});
