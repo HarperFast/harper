a6095aab2680ed1417da0b0354d64204d59be386
samj <sam@harperdb.io>
2019-10-15 16:56:22 -0600

    CORE-547 Adds fix for heDropTable to only delete attrs in schema_table and final heDropSchema code

--- data_layer/harperBridge/heBridge/heMethods/heDropSchema.js
+++ data_layer/harperBridge/heBridge/heMethods/heDropSchema.js
@@ -4,35 +4,49 @@ const heDropTable = require('./heDropTable');
 const heDeleteRecords = require('./heDeleteRecords');
 const heGetDataByValue = require('./heGetDataByValue');
 // const moveFolderToTrash = require('../fsUtility/moveFolderToTrash');
-const deleteAttrStructure = require('../../fsBridge/fsUtility/deleteAttrStructure');
-const env = require('../../../../utility/environment/environmentManager');
+// const deleteAttrStructure = require('../../fsBridge/fsUtility/deleteAttrStructure');
+// const env = require('../../../../utility/environment/environmentManager');
 const hdb_terms = require('../../../../utility/hdbTerms');
+const common_utils = require('../../../../utility/common_utils');
 
-const DATE_SUBSTR_LENGTH = 19;
-let current_date = new Date().toISOString().substr(0, DATE_SUBSTR_LENGTH);
+// const DATE_SUBSTR_LENGTH = 19;
+// let current_date = new Date().toISOString().substr(0, DATE_SUBSTR_LENGTH);
 // const TRASH_BASE_PATH = `${env.getHdbBasePath()}/${terms.HDB_TRASH_DIR}`;
 
 module.exports = heDropSchema;
 
 async function heDropSchema(drop_schema_obj) {
     let schema = drop_schema_obj.schema;
-    let delete_schema_obj = {
-        schema: drop_schema_obj.schema,
-        table: ''
-    };
 
     try {
+        let delete_schema_obj = {
+            schema: hdb_terms.SYSTEM_SCHEMA_NAME,
+            table: hdb_terms.SYSTEM_TABLE_NAMES.SCHEMA_TABLE_NAME,
+            hash_values: [drop_schema_obj.schema]
+        };
+
+        const delete_response = heDeleteRecords(delete_schema_obj);
+
+        if (delete_response.deleted_hashes.length === 0) {
+            throw new Error(`schema '${drop_schema_obj.schema}' does not exist`);
+        }
+
         let tables = global.hdb_schema[schema];
+        let delete_table_obj = {
+            schema: schema,
+            table: ''
+        };
+
         for (let table_name in tables) {
-            delete_schema_obj.table = tables[table_name].name;
+            delete_table_obj.table = tables[table_name].name;
             try {
-                heDropTable(delete_schema_obj);
+                heDropTable(delete_table_obj);
             } catch(e) {
                 throw e;
             }
         }
 
-        dropSchemaFromSystem(drop_schema_obj);
+        // dropSchemaFromSystem(drop_schema_obj);
     } catch(err) {
         throw err;
     }
@@ -42,43 +56,16 @@ async function heDropSchema(drop_schema_obj) {
  * Searches the system schema for the schema hash, then uses hash to delete schema from system.
  * @param drop_schema_obj
  */
-function dropSchemaFromSystem(drop_schema_obj) {
-    let search_obj = {
-        schema: hdb_terms.SYSTEM_SCHEMA_NAME,
-        table: hdb_terms.SYSTEM_TABLE_NAMES.SCHEMA_TABLE_NAME,
-        search_attribute: hdb_terms.SYSTEM_DEFAULT_ATTRIBUTE_NAMES.ATTR_NAME_KEY,
-        search_value: drop_schema_obj.schema,
-        get_attributes: [hdb_terms.SYSTEM_DEFAULT_ATTRIBUTE_NAMES.ATTR_NAME_KEY]
-    };
-    let search_result;
-    let delete_schema;
-
-    try {
-        search_result = heGetDataByValue(search_obj);
-    } catch(err) {
-        throw err;
-    }
-
-    // Data found by the search function should match the drop_schema_object
-    for (let item in search_result) {
-        if (search_result[item].name === drop_schema_obj.schema) {
-            delete_schema = search_result[item];
-        }
-    }
-
-    if (!delete_schema) {
-        throw new Error(`${drop_schema_obj.schema} was not found`);
-    }
-
-    let delete_schema_obj = {
-        schema: hdb_terms.SYSTEM_SCHEMA_NAME,
-        table: hdb_terms.SYSTEM_TABLE_NAMES.SCHEMA_TABLE_NAME,
-        // hash_attribute: hdb_terms.SYSTEM_SCHEMA_NAME,
-        hash_values: [delete_schema[hdb_terms.SYSTEM_DEFAULT_ATTRIBUTE_NAMES.ATTR_NAME_KEY]]
-    };
-    try {
-        heDeleteRecords(delete_schema_obj);
-    } catch(err) {
-        throw err;
-    }
-}
+// function dropSchemaFromSystem(drop_schema_obj) {
+//     let delete_schema_obj = {
+//         schema: hdb_terms.SYSTEM_SCHEMA_NAME,
+//         table: hdb_terms.SYSTEM_TABLE_NAMES.SCHEMA_TABLE_NAME,
+//         hash_values: [drop_schema_obj.schema]
+//     };
+//
+//     try {
+//         return heDeleteRecords(delete_schema_obj);
+//     } catch(err) {
+//         throw err;
+//     }
+// }
