df1b94ecb974288010807ac3daf5770d80a6c699
samj <sam@harperdb.io>
2019-11-08 10:43:29 -0700

    CORE-670 - WIP - updates to getAttributeFileValues to use better read/scan logic

--- data_layer/harperBridge/fsBridge/fsUtility/getAttributeFileValues.js
+++ data_layer/harperBridge/fsBridge/fsUtility/getAttributeFileValues.js
@@ -6,8 +6,12 @@ const fs = require('fs-extra');
 const common_utils = require('../../../../utility/common_utils');
 const getBasePath = require('./getBasePath');
 const hdb_terms = require('../../../../utility/hdbTerms');
+const log = require('../../../../utility/logging/harper_logger');
 
 module.exports = getAttributeFileValues;
+
+const RAW_FILE_READ_LIMIT = 1000;
+const BLOB_FOLDER_NAME = 'blob';
 
 async function getAttributeFileValues(get_attributes, search_object, hash_attr, hash_results) {
     try {
@@ -28,24 +32,77 @@ async function getAttributeFileValues(get_attributes, search_object, hash_attr, 
             return attributes_data;
         }
 
-        for (const attribute of get_attributes) {
-            //evaluate if an array of strings or objects has been passed in and assign values accordingly
-            const attribute_name = (typeof attribute === 'string') ? attribute : attribute.attribute;
-            const is_hash = attribute_name === hash_attr;
-            //if attribute is the hash value, assign hash_result values to hash
-            if (is_hash) {
-                let hash_attr_data = {};
-                for (const file of hash_values) {
-                    hash_attr_data[file] = common_utils.autoCast(file);
+        if (hash_values.length > RAW_FILE_READ_LIMIT) {
+            //TODO - add logic for scanning attr value dir
+            //START
+            let blob_paths = {};
+            await Promise.all(get_attributes.map(async attr => {
+                try {
+                    // let sub_path = common_utils.buildFolderPath(schema, table, attr);
+
+                    let attribute_path = common_utils.buildFolderPath(table_path, attr);
+                    let results = await fs.readdir(attribute_path);
+                    await Promise.all(results.map(async value => {
+                        try {
+                            //TODO - use new approach to cleaning up values and ids below
+                            let the_value = common_utils.autoCast(value.replace(escaped_slash_regex, '/'));
+                            let ids = await fs.readdir(common_utils.buildFolderPath(attribute_path,value));
+                            for (let id of ids) {
+                                if (id===BLOB_FOLDER_NAME) {
+                                    //TODO - should probably just do a deeper search here instead of add to the blob obj
+                                    let blob_path = common_utils.buildFolderPath(sub_path, value);
+                                    if (!blob_paths[blob_path]) {
+                                        blob_paths[blob_path] = column;
+                                    }
+                                } else {
+                                    //TODO - use new approach to cleaning up values and ids below
+
+                                    let the_id = common_utils.autoCast(this._stripFileExtension(id));
+                                    let the_key = this.data[`${column.table.databaseid}_${column.table.tableid}`].__merged_data[the_id];
+                                    if (the_key) {
+                                        this.data[`${column.table.databaseid}_${column.table.tableid}`].__merged_data[the_id][column.attribute] = the_value;
+                                    }
+                                }
+                            }
+                        } catch (e) {
+                            log.error(e);
+                        }
+
+                        if (!_.isEmpty(blob_paths)) {
+                            await this._readBlobFiles(blob_paths).catch(e => {
+                                log.error(e);
+                            });
+                        }
+                    }));
+                } catch (e) {
+                    log.error(e);
                 }
-                attributes_data[attribute_name] = hash_attr_data;
-            } else {
-                const attribute_file_values = await readAttributeFiles(table_path, attribute_name, hash_values);
-                if (!_.isEmpty(attribute_file_values)) {
-                    attributes_data[attribute_name] = attribute_file_values;
+
+            }));
+
+
+            //END
+        } else {
+            for (const attribute of get_attributes) {
+                //evaluate if an array of strings or objects has been passed in and assign values accordingly
+                const attribute_name = (typeof attribute === 'string') ? attribute : attribute.attribute;
+                const is_hash = attribute_name === hash_attr;
+                //if attribute is the hash value, assign hash_result values to hash
+                if (is_hash) {
+                    let hash_attr_data = {};
+                    for (const file of hash_values) {
+                        hash_attr_data[file] = common_utils.autoCast(file);
+                    }
+                    attributes_data[attribute_name] = hash_attr_data;
+                } else {
+                    const attribute_file_values = await readAttributeFiles(table_path, attribute_name, hash_values);
+                    if (!_.isEmpty(attribute_file_values)) {
+                        attributes_data[attribute_name] = attribute_file_values;
+                    }
                 }
             }
         }
+
         return attributes_data;
     } catch(err) {
         throw err;
@@ -86,6 +143,18 @@ async function readAttributeFiles(table_path, attribute, hash_files, is_hash) {
 }
 
 async function validateHashValuesExist(table_path, hash_attr, hash_files) {
-    const valid_hashes = await readAttributeFiles(table_path, hash_attr, hash_files, true);
-    return Object.values(valid_hashes);
+    // const valid_hashes = await readAttributeFiles(table_path, hash_attr, hash_files, true);
+    // return Object.values(valid_hashes);
+    const hash_path = common_utils.buildFolderPath(table_path, hdb_terms.HASH_FOLDER_NAME, hash_attr);
+    let existing_values = [];
+    await Promise.all(hash_files.map(async value => {
+        try {
+            await fs.access(common_utils.buildFolderPath(hash_path, value + hdb_terms.HDB_FILE_SUFFIX), fs.constants.F_OK);
+            existing_values.push(value);
+        } catch (e) {
+            log.error(e);
+            // no-op
+        }
+    }));
+    return existing_values;
 }
