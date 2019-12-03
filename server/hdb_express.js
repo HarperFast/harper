d3915dba3ddae7c0424cb5cb2ed80ed797fd74ee
deliciousmonster <jaxon@deliciousmonster.com>
2019-12-03 14:21:26 -0700

    Update hdb_express.js

--- server/hdb_express.js
+++ server/hdb_express.js
@@ -372,7 +372,13 @@ if (cluster.isMaster &&( numCPUs >= 1 || DEBUG )) {
     // This handles all get requests for the studio
     app.use(compression());
     app.use(express.static(guidePath.join(__dirname,'../docs')));
-    app.get('*', function (req, res) { res.sendFile(guidePath.join(__dirname,'../docs', 'index.html')); });
+    app.get('/processCSV.worker.js', function (req, res) {
+        res.type('text/javascript');
+        res.sendFile(guidePath.join(__dirname,'../docs', 'processCSV.worker.js'));
+    });
+    app.get('*', function (req, res) {
+        res.sendFile(guidePath.join(__dirname,'../docs', 'index.html'));
+    });
 
     // Recent security posts recommend disabling this header.
     app.disable('x-powered-by');
