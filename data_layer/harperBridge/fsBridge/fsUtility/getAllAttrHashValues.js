4c01c30bd91ebb46947e5144e35549cd705dc093
DavidCockerill <david@harperdb.io>
2020-01-02 14:53:12 -0700

    CORE-833 add ENOENT error check to getAllAttrHashValues

--- data_layer/harperBridge/fsBridge/fsUtility/getAllAttrHashValues.js
+++ data_layer/harperBridge/fsBridge/fsUtility/getAllAttrHashValues.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const _ = require('lodash');
 const fs = require('fs-extra');
 
 const common_utils = require('../../../../utility/common_utils');
 const log = require('../../../../utility/logging/harper_logger');
+const hdb_terms = require('../../../../utility/hdbTerms');
 
 module.exports = getAllAttrHashValues;
 
@@ -14,13 +14,15 @@ async function getAllAttrHashValues(hash_dir_path) {
         const hash_results = await fs.readdir(hash_dir_path);
         if (common_utils.isEmptyOrZeroLength(hash_results)) {
             return final_hash_results;
-        } else {
-            for (let i = 0; i < hash_results.length; i++) {
-                final_hash_results.push(common_utils.autoCast(common_utils.stripFileExtension(hash_results[i])));
-            }
+        }
+
+        for (let i = 0; i < hash_results.length; i++) {
+            final_hash_results.push(common_utils.autoCast(common_utils.stripFileExtension(hash_results[i])));
         }
     } catch(e) {
-        log.error(e);
+        if (e.code !== hdb_terms.NODE_ERROR_CODES.ENOENT) {
+            log.error(e);
+        }
     }
 
     return final_hash_results;
