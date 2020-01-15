3cf082655326144031d6c0fe8574c6f173c0b1cb
Kyle Bernhardy <kyle@harperdb.io>
2020-01-15 12:11:01 -0700

    added deleteUtility.js

---
+++ utility/lmdb/deleteUtility.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const environment_util = require('./environmentUtility');
+const lmdb = require('node-lmdb');
+
+function deleteRecords(env, hash_attribute, hash_values){
+    //
+}
