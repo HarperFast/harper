4398e4fa7dcd47d0926422f121112531f180f629
Kyle Bernhardy <kyle@harperdb.io>
2020-01-22 20:22:48 -0700

    Revert "implemented the lmdbCreateSchema.js module which adds the meta data for the schema into the system.hdb_schema environment also creates the folder to put any future environments"
    This reverts commit b3d7db68

--- data_layer/harperBridge/lmdbBridge/lmdbMethods/lmdbCreateSchema.js
+++ data_layer/harperBridge/lmdbBridge/lmdbMethods/lmdbCreateSchema.js
@@ -1,43 +1,11 @@
 'use strict';
 
-const hdb_terms = require('../../../../utility/hdbTerms');
-const lmdb_create_records = require('./lmdbCreateRecords');
-const fs = require('fs-extra');
-const path = require('path');
-const terms = require('../../../../utility/hdbTerms');
-const env = require('../../../../utility/environment/environmentManager');
-
-if(!env.isInitialized()){
-    env.initSync();
-}
-
-const BASE_SCHEMA_PATH = path.join(env.getHdbBasePath(), terms.SCHEMA_DIR_NAME);
-
 module.exports = lmdbCreateSchema;
 
 /**
  * creates the meta data for the schema
  * @param create_schema_obj
  */
-async function lmdbCreateSchema(create_schema_obj) {
-    let insert_object = {
-        operation: hdb_terms.OPERATIONS_ENUM.INSERT,
-        schema: hdb_terms.SYSTEM_SCHEMA_NAME,
-        table: hdb_terms.SYSTEM_TABLE_NAMES.SCHEMA_TABLE_NAME,
-        records: [
-            {
-                name: create_schema_obj.schema,
-                createddate: '' + Date.now()
-            }
-        ]
-    };
+function lmdbCreateSchema(create_schema_obj) {
 
-    try {
-        let results = await lmdb_create_records(insert_object);
-        if(results.written_hashes.length > 0){
-            await fs.mkdirp(path.join(BASE_SCHEMA_PATH, create_schema_obj.schema));
-        }
-    } catch(err) {
-        throw err;
-    }
 }
