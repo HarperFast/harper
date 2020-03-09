f9a265002a8cd9ca0fe8392896264e93e8185224
Kyle Bernhardy <kyle@harperdb.io>
2020-03-09 15:28:22 -0600

    CORE-944
    Removed daily_api_calls_current & daily_api_calls_limit from the response of registration_info

--- utility/registration/registrationHandler.js
+++ utility/registration/registrationHandler.js
@@ -161,13 +161,10 @@ async function getRegistrationInfo() {
         version: null,
         storage_type: null,
         ram_allocation: null,
-        license_expiration_date: null,
-        daily_api_calls_current: null,
-        daily_api_calls_limit: null
+        license_expiration_date: null
     };
 
     let license;
-    let current_api_calls;
 
     try {
         license = await hdb_license.getLicense();
@@ -180,20 +177,11 @@ async function getRegistrationInfo() {
         throw new Error('There were no licenses found.');
     }
 
-    try {
-        current_api_calls = await apiLimiter.getDailyAPICalls();
-    } catch(e) {
-        log.error(`There was an error when calculating daily api calls due to: ${e.message}`);
-        throw e;
-    }
-
     reg_info_obj.registered = license.enterprise;
     reg_info_obj.version = version.version();
     reg_info_obj.storage_type = license.storage_type;
     reg_info_obj.ram_allocation = license.ram_allocation;
     reg_info_obj.license_expiration_date = license.enterprise ? license.exp_date : null;
-    reg_info_obj.daily_api_calls_limit = license.api_call;
-    reg_info_obj.daily_api_calls_current = current_api_calls._consumedPoints;
 
     return reg_info_obj;
 }
