2f2691477bb2ff14d796e9f500e7e9c88e3f7ab1
Kyle Bernhardy <kyle@harperdb.io>
2020-04-15 15:38:19 -0600

    CORE-994
    Added new operation "system_information".  This operation returns info & metrics for the OS, time, memory, cpu, disk, network
    
    the operation optionally receives an attributes param which will only grab specific top level attributes.

---
+++ utility/environment/SystemInformationOperation.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const terms = require('../hdbTerms');
+
+class SystemInformationOperation{
+    /**
+     * @param {Array<String>} attributes
+     */
+    constructor(attributes) {
+        this.operator = terms.OPERATIONS_ENUM.SYSTEM_INFORMATION;
+        this.attributes = attributes;
+    }
+}
+
+module.exports = SystemInformationOperation;
