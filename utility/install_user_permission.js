6f520d4d1d4fb3210bd132719c666989245b9eba
samj <sam@harperdb.io>
2020-06-17 16:47:30 -0600

    CORE-1046 Updates check_permissions file to install_user_permission to make naming convention more specific

--- utility/install_user_permission.js
+++ utility/install_user_permission.js
@@ -2,10 +2,10 @@ const os = require("os");
 const env = require('../utility/environment/environmentManager');
 
 module.exports = {
-    checkPermission: checkPermission,
+    checkPermission
 };
 
-function checkPermission () {
+function checkPermission() {
     if(os.userInfo().username !== env.get('install_user')){
         throw new Error(`Error: Must execute as ${env.get('install_user')}`);
     }
