c0a4d2d044c8cb838490f8e09761ffed83a2e33b
Kyle <kyle@harperdb.io>
2020-09-14 10:22:26 -0600

    started work on create_token function

--- security/tokenAuthentication.js
+++ security/tokenAuthentication.js
@@ -1,2 +1,22 @@
 'use strict';
 
+const jwt = require('jsonwebtoken');
+const {promisify} = require('util');
+const auth = require('./auth');
+const p_find_validate_user = promisify(auth.findAndValidateUser);
+
+async function createAuthToken(auth_object){
+    //validate auth_object
+
+    //query for user/pw
+    let user = await p_find_validate_user(auth_object.username, auth_object.password);
+    if(!user){
+        //throw error
+    }
+
+    //get rsa key
+
+    //sign tokens
+
+    //return json
+}
