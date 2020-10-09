e6ee269601908387b1baa0ef7ccf5767fd6f05e4
samj <sam@harperdb.io>
2020-10-09 09:58:40 -0600

    CORE-1129 Fixes code smell

--- data_layer/harperBridge/lmdbBridge/lmdbMethods/lmdbUpsertRecords.js
+++ data_layer/harperBridge/lmdbBridge/lmdbMethods/lmdbUpsertRecords.js
@@ -21,41 +21,38 @@ module.exports = lmdbUpsertRecords;
  * @returns {{ skipped_hashes: *, written_hashes: *, schema_table: *, new_attributes: *, txn_time: * }}
  */
 async function lmdbUpsertRecords(upsert_obj) {
-    try {
-        let { schema_table, attributes } = insert_update_validate(upsert_obj);
+    let { schema_table, attributes } = insert_update_validate(upsert_obj);
 
-        lmdb_process_rows(upsert_obj, attributes, schema_table.hash_attribute);
+    lmdb_process_rows(upsert_obj, attributes, schema_table.hash_attribute);
 
-        if (upsert_obj.schema !== hdb_terms.SYSTEM_SCHEMA_NAME) {
-            if (!attributes.includes(hdb_terms.TIME_STAMP_NAMES_ENUM.CREATED_TIME)) {
-                attributes.push(hdb_terms.TIME_STAMP_NAMES_ENUM.CREATED_TIME);
-            }
-
-            if (!attributes.includes(hdb_terms.TIME_STAMP_NAMES_ENUM.UPDATED_TIME)) {
-                attributes.push(hdb_terms.TIME_STAMP_NAMES_ENUM.UPDATED_TIME);
-            }
+    if (upsert_obj.schema !== hdb_terms.SYSTEM_SCHEMA_NAME) {
+        if (!attributes.includes(hdb_terms.TIME_STAMP_NAMES_ENUM.CREATED_TIME)) {
+            attributes.push(hdb_terms.TIME_STAMP_NAMES_ENUM.CREATED_TIME);
         }
 
-        let new_attributes = await lmdb_check_new_attributes(upsert_obj.hdb_auth_header, schema_table, attributes);
-        let env_base_path = path.join(getBaseSchemaPath(), upsert_obj.schema.toString());
-        let environment = await environment_utility.openEnvironment(env_base_path, upsert_obj.table);
-        let lmdb_response = lmdb_upsert_records(environment, schema_table.hash_attribute, attributes, upsert_obj.records);
-
-        //TODO - will be wired in as part of CORE-1142
-        //try {
-        //     await write_transaction(upsert_obj, lmdb_response);
-        // }catch(e){
-        //     logger.error(`unable to write transaction due to ${e.message}`);
-        // }
-
-        return {
-            written_hashes: lmdb_response.written_hashes,
-            skipped_hashes: lmdb_response.skipped_hashes,
-            schema_table,
-            new_attributes,
-            txn_time: lmdb_response.txn_time
-        };
-    } catch(err) {
-        throw err;
+        if (!attributes.includes(hdb_terms.TIME_STAMP_NAMES_ENUM.UPDATED_TIME)) {
+            attributes.push(hdb_terms.TIME_STAMP_NAMES_ENUM.UPDATED_TIME);
+        }
     }
+
+    let new_attributes = await lmdb_check_new_attributes(upsert_obj.hdb_auth_header, schema_table, attributes);
+    let env_base_path = path.join(getBaseSchemaPath(), upsert_obj.schema.toString());
+    let environment = await environment_utility.openEnvironment(env_base_path, upsert_obj.table);
+    let lmdb_response = lmdb_upsert_records(environment, schema_table.hash_attribute, attributes, upsert_obj.records);
+
+    //TODO - will be wired in as part of CORE-1142
+    //try {
+    //     await write_transaction(upsert_obj, lmdb_response);
+    // }catch(e){
+    //     logger.error(`unable to write transaction due to ${e.message}`);
+    // }
+
+    return {
+        written_hashes: lmdb_response.written_hashes,
+        skipped_hashes: lmdb_response.skipped_hashes,
+        schema_table,
+        new_attributes,
+        txn_time: lmdb_response.txn_time
+    };
+
 }
