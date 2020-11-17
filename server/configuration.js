9b760388e0b7d475ce7d0a1f08b7ac72c0e8869b
Kyle <kyle@harperdb.io>
2020-11-16 19:31:39 -0700

    added getConfig function

---
+++ server/configuration.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const HDB_SETTINGS_KEYS = Object.keys(require('../utility/hdbTerms').HDB_SETTINGS_NAMES_REVERSE_LOOKUP);
+const env = require('../utility/environment/environmentManager');
+if(!env.isInitialized()){
+    env.initSync();
+}
+
+module.exports = {
+    getConfig
+};
+
+/**
+ * this function returns all of the config settings
+ * @returns {{}}
+ */
+function getConfig(){
+    let result = {};
+    for(let x = 0, length = HDB_SETTINGS_KEYS.length; x < length; x++){
+        let key = HDB_SETTINGS_KEYS[x];
+        let value = env.getProperty(key);
+        result[key] = value;
+    }
+
+    return result;
+}
