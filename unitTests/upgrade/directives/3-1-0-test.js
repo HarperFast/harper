4ae3cd502df36d2e9db22740763222d09639b3d2
DavidCockerill <david@harperdb.io>
2021-05-07 11:53:45 -0600

    CORE-1292 finish unit tests

--- unitTests/upgrade/directives/3-1-0-test.js
+++ unitTests/upgrade/directives/3-1-0-test.js
@@ -3,12 +3,16 @@
 const chai = require('chai');
 const sinon = require('sinon');
 const { expect } = chai;
+const sinon_chai = require('sinon-chai');
+chai.use(sinon_chai);
 const rewire = require('rewire');
 const test_util = require('../../../unitTests/test_utils');
 const hdb_logger = require('../../../utility/logging/harper_logger');
 const hdb_terms = require('../../../utility/hdbTerms');
+const { buildFile, getSettingsFilePath } = require('../../settingsTestFile');
 const fs = require('fs-extra');
 
+const SETTINGS_PATH = getSettingsFilePath();
 let directive3_1_0;
 let updateSettingsFile_3_1_0;
 
@@ -23,34 +27,24 @@ describe('Test 3.1.0 Upgrade Directive', () => {
 
     after(() => {
         sandbox.restore();
+        fs.unlinkSync(SETTINGS_PATH);
     });
 
-/*    describe('Test getOldPropsValue function', () => {
-        const directive3_1_0_file = rewire('../../../upgrade/directives/3-1-0');
-        const getOldPropsValue = directive3_1_0_file.__get__('getOldPropsValue');
-
-        it('Test when value is not empty', () => {
-            const result = getOldPropsValue('IPC_SERVER_PORT');
-            expect(result).to.equal('')
-        });
-        
-    });*/
-    
     describe('Test updateSettingsFile_3_1_0 function', () => {
-        let consoleLog_stub;
         let consoleError_stub;
-        let logInfo_stub;
+        let logError_stub;
         let fsWriteFileSync_stub;
         let fsCopySync_stub;
-        let fsUnlinkSync_stub;
 
         before(() => {
-            consoleLog_stub = sandbox.stub(console, 'log');
+            buildFile();
+            sandbox.stub(console, 'log');
+            sandbox.stub(fs, 'unlinkSync');
+            sandbox.stub(hdb_logger, 'info');
             consoleError_stub = sandbox.stub(console, 'error');
-            logInfo_stub = sandbox.stub(hdb_logger, 'info');
+            logError_stub = sandbox.stub(hdb_logger, 'error');
             fsWriteFileSync_stub = sandbox.stub(fs, 'writeFileSync');
             fsCopySync_stub = sandbox.stub(fs, 'copySync');
-            fsUnlinkSync_stub = sandbox.stub(fs, 'unlinkSync');
         });
 
         afterEach(() => {
@@ -61,21 +55,35 @@ describe('Test 3.1.0 Upgrade Directive', () => {
             sandbox.restore();
         });
         
-        it('Test new setting file has IPC port and logger removed', () => {
+        it('Test new setting file has IPC port param and logger param is removed', () => {
             const result = updateSettingsFile_3_1_0();
-
             const new_settings_val = fsWriteFileSync_stub.args[0][1];
             for (const name in hdb_terms.HDB_SETTINGS_NAMES) {
                 const setting_name = hdb_terms.HDB_SETTINGS_NAMES[name];
-                if (setting_name === 'settings_path' || setting_name === 'install_user') continue;
+                if (setting_name === 'settings_path' || setting_name === 'install_user' || setting_name === 'LOGGER') continue;
                 expect(new_settings_val).to.include(setting_name, `Expected new setting to contain ${setting_name} but it did not`);
             }
 
-
-            console.log(result);
-            
+            expect(new_settings_val).to.not.include('LOGGER');
+            expect(result).to.equal('New settings file for 3.1.0 upgrade successfully created.');
+            expect(fsCopySync_stub).to.have.been.called;
+            expect(fsWriteFileSync_stub).to.have.been.called;
         });
 
-    });
+        it('Test error from backup is logged and thrown', () => {
+            fsCopySync_stub.throws(new Error('Error with backup'));
+            test_util.assertErrorSync(updateSettingsFile_3_1_0, [], new Error('Error with backup'));
+            expect(logError_stub.args[0][0].message).to.equal('Error with backup');
+            expect(consoleError_stub).to.have.been.calledWith('There was a problem writing the backup for the old settings file.  Please check the log for details.');
+            fsCopySync_stub.returns();
+        });
 
+        it('Test error from writing new setting file is logged and thrown', () => {
+            fsWriteFileSync_stub.throws(new Error('Error writing new file'));
+            test_util.assertErrorSync(updateSettingsFile_3_1_0, [], new Error('Error writing new file'));
+            expect(consoleError_stub).to.have.been.calledWith('There was a problem writing the new settings file. Please check the log for details.');
+            expect(logError_stub).to.have.been.called;
+            expect(fsCopySync_stub).to.have.been.called;
+        });
+    });
 });
