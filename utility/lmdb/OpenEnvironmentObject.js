d8e3251ee65551a6cec36d471935cc59261daab1
Kyle Bernhardy <kyle@harperdb.io>
2022-02-03 13:20:59 -0700

    updated version of lmdb due to dramatic performance improvements

--- utility/lmdb/OpenEnvironmentObject.js
+++ utility/lmdb/OpenEnvironmentObject.js
@@ -7,7 +7,7 @@ class OpenEnvironmentObject {
 		this.maxDbs = max_dbs;
 		this.maxReaders = max_readers;
 		this.sharedStructuresKey = Symbol.for('structures');
-		this.noSync = no_sync;
+		this.overlappingSync = !no_sync;
 	}
 }
 
