49d2acfde09364ed0b8820af49e7e9b17e2c10aa
Kyle Bernhardy <kyle@harperdb.io>
2022-03-01 08:10:55 -0700

    test change

--- utility/lmdb/OpenEnvironmentObject.js
+++ utility/lmdb/OpenEnvironmentObject.js
@@ -7,7 +7,7 @@ class OpenEnvironmentObject {
 		this.maxDbs = max_dbs;
 		this.maxReaders = max_readers;
 		this.sharedStructuresKey = Symbol.for('structures');
-		this.overlappingSync = true;
+		this.overlappingSync = false;
 	}
 }
 
