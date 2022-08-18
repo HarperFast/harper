8179cc93764fbc67e573bd9c47778c96da2cf5e0
Devin Holland <50112339+Devin-Holland@users.noreply.github.com>
2022-08-18 15:40:04 -0400

    Make ci tests and unit tests heredocs into scripts in the repo (#1272)
    * Make ci tests and unit tests heredocs into scripts in the repo
    
    * Move chmod to right before commands run

---
+++ test/ci-tests/ci-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+. /home/ubuntu/.nvm/nvm.sh
+. /home/ubuntu/.nvm/bash_completion
+
+npm --loglevel=error install -g newman
+npm --loglevel=error install -g newman-reporter-html
+npm --loglevel=error install -g newman-reporter-htmlextra
+
+cd /home/ubuntu/harperdb/integrationTests
+
+# Correct path to CSV files for integration tests
+sed -i 's/\/usr\/csv\//\/home\/ubuntu\/harperdb\/test\/data\/integrationTestsCsvs\//g' Int_test_env_var.json
+cat Int_test_env_var.json | grep "integrationTestsCsvs"
+
+newman run HarperDB_Integration_Tests.json -e Int_test_env_var.json --reporters cli,html,htmlextra --reporter-html-export newman/report.html --reporter-htmlextra-export newman/extra_report.html --insecure
