af9856f8b12cb56c14f073d5e761cfd073ee3732
David C <48922884+DavidCockerill@users.noreply.github.com>
2023-01-27 10:27:51 -0500

    Merge 4.0.3 into main (#1448)

--- launchServiceScripts/launchUpdateNodes4-0-0.js
+++ launchServiceScripts/launchUpdateNodes4-0-0.js
@@ -1,13 +1,6 @@
 'use strict';
 
-const is_compiled = process.env.HDB_COMPILED === 'true';
-let update_nodes_4_0_0;
-if (is_compiled) {
-	require('bytenode');
-	update_nodes_4_0_0 = require('../upgrade/nats/updateNodes4-0-0.jsc');
-} else {
-	update_nodes_4_0_0 = require('../upgrade/nats/updateNodes4-0-0');
-}
+const update_nodes_4_0_0 = require('../upgrade/nats/updateNodes4-0-0');
 
 (async () => {
 	try {
--- utility/devops/build/build.js
+++ utility/devops/build/build.js
@@ -17,6 +17,7 @@ let entry_modules = [
 	'launchServiceScripts/launchInstallNATSServer.js',
 	'launchServiceScripts/launchNatsIngestService.js',
 	'launchServiceScripts/launchNatsReplyService.js',
+	'launchServiceScripts/launchUpdateNodes4-0-0.js',
 	'server/jobs/jobProcess.js',
 	'server/threads/thread-http-server.js',
 	'utility/scripts/restartHdb.js',
