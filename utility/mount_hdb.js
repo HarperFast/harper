39d3140a97192d18658512770c9acd62b2002784
David C <48922884+DavidCockerill@users.noreply.github.com>
2023-05-30 23:45:52 -0400

    CORE-2183 Fix mount_hdb-test.js after resource-api changes (#1547)

--- utility/mount_hdb.js
+++ utility/mount_hdb.js
@@ -5,7 +5,6 @@ const path = require('path');
 const terms = require('../utility/hdbTerms');
 const hdb_logger = require('../utility/logging/harper_logger');
 const bridge = require('../dataLayer/harperBridge/harperBridge');
-const lmdb_environment_utility = require('../utility/lmdb/environmentUtility');
 const system_schema = require('../json/systemSchema');
 const init_paths = require('../dataLayer/harperBridge/lmdbBridge/lmdbUtility/initializePaths');
 
@@ -35,45 +34,24 @@ async function mountHdb(hdb_path) {
  */
 async function createLMDBTables() {
 	// eslint-disable-next-line global-require
-	let lmdb_create_table;
-	// eslint-disable-next-line global-require
 	const CreateTableObject = require('../dataLayer/CreateTableObject');
 
 	let tables = Object.keys(system_schema);
 
 	for (let x = 0; x < tables.length; x++) {
 		let table_name = tables[x];
-		let table_env;
 		let hash_attribute = system_schema[table_name].hash_attribute;
 		try {
-			const schema_path = init_paths.initSystemSchemaPaths(terms.SYSTEM_SCHEMA_NAME, table_name);
+			init_paths.initSystemSchemaPaths(terms.SYSTEM_SCHEMA_NAME, table_name);
 			let create_table = new CreateTableObject(terms.SYSTEM_SCHEMA_NAME, table_name, hash_attribute);
 			create_table.attributes = system_schema[table_name].attributes;
 			let primary_key_attribute = create_table.attributes.find(({ attribute }) => attribute === hash_attribute);
 			primary_key_attribute.isPrimaryKey = true;
-			let table = await bridge.createTable(table_name, create_table);
+			await bridge.createTable(table_name, create_table);
 		} catch (e) {
 			hdb_logger.error(`issue creating environment for ${terms.SYSTEM_SCHEMA_NAME}.${table_name}: ${e}`);
 			throw e;
 		}
-		/*
-		//create all dbis
-		let attributes = system_schema[table_name].attributes;
-		for (let y = 0; y < attributes.length; y++) {
-			let attribute_name = attributes[y].attribute;
-			try {
-				if (terms.TIME_STAMP_NAMES.indexOf(attribute_name) >= 0) {
-					await lmdb_environment_utility.createDBI(table_env, attribute_name, true);
-				} else if (attribute_name === hash_attribute) {
-					await lmdb_environment_utility.createDBI(table_env, attribute_name, false, true);
-				} else {
-					await lmdb_environment_utility.createDBI(table_env, attribute_name, true, false);
-				}
-			} catch (e) {
-				hdb_logger.error(`issue creating dbi for ${terms.SYSTEM_SCHEMA_NAME}.${table_name}.${attribute_name}: ${e}`);
-				throw e;
-			}
-		}*/
 	}
 }
 
