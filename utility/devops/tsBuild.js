37a2d633017af42f1a1628c1c5747cb608010c97
Kris Zyp <kriszyp@gmail.com>
2023-07-18 17:13:03 -0600

    Do run tsc watch if no ts-build directory exists

--- utility/devops/tsBuild.js
+++ utility/devops/tsBuild.js
@@ -15,56 +15,68 @@ if (is_source_code) {
 		/**
 		 * Check to see if any TypeScript files need to be recompiled
 		 */
-		for (let filename of fg.sync(
-			SRC_DIRECTORIES.map((dir) => dir + '/**/*.ts'),
-			{ cwd: PACKAGE_ROOT }
-		)) {
-			let source_time = 0;
-			let compiled_time = 0;
-			try {
-				// Note that we subtract a few seconds from the source file timestamp because it seems like WebStorm
-				// compiles and then saves the source file, so it can have a slightly newer timestamp
-				source_time = statSync(join(PACKAGE_ROOT, filename)).mtimeMs - 5000;
-				compiled_time = statSync(join(PACKAGE_ROOT, TS_DIRECTORY, filename.replace(/.ts$/, '.js'))).mtimeMs;
-			} catch (error) {}
+		let target_directory_existed;
+		try {
+			statSync(join(PACKAGE_ROOT, TS_DIRECTORY));
+			target_directory_existed = true;
+		} catch (error) {}
+		if (target_directory_existed) {
+			for (let filename of fg.sync(
+				SRC_DIRECTORIES.map((dir) => dir + '/**/*.ts'),
+				{ cwd: PACKAGE_ROOT }
+			)) {
+				let source_time = 0;
+				let compiled_time = 0;
+				try {
+					// Note that we subtract a few seconds from the source file timestamp because it seems like WebStorm
+					// compiles and then saves the source file, so it can have a slightly newer timestamp
+					source_time = statSync(join(PACKAGE_ROOT, filename)).mtimeMs - 5000;
+					compiled_time = statSync(join(PACKAGE_ROOT, TS_DIRECTORY, filename.replace(/.ts$/, '.js'))).mtimeMs;
+				} catch (error) {}
 
-			if (source_time > compiled_time) {
-				console.warn(
-					`TypeScript ${filename} is not compiled` +
-						(compiled_time
-							? ` (TS source file was modified at ${new Date(source_time)} and compiled file at ${new Date(
-									compiled_time
-							  )})`
-							: '') +
-						`, consider enabling auto-compilation of TypeScript in your IDE), compiling now.`
-				);
-				needs_compile = true;
-				break;
+				if (source_time > compiled_time) {
+					console.warn(
+						`TypeScript ${filename} is not compiled` +
+							(compiled_time
+								? ` (TS source file was modified at ${new Date(source_time)} and compiled file at ${new Date(
+										compiled_time
+								)})`
+								: '') +
+							`, consider enabling auto-compilation of TypeScript in your IDE), compiling now.`
+					);
+					needs_compile = true;
+					break;
+				}
 			}
+		} else {
+			console.log('TypeScript modules are not compiled, compiling now');
+			needs_compile = true;
 		}
 		if (needs_compile) {
 			// if we need it, run typescript compiler
 			let result = spawnSync(process.argv[0], [join(PACKAGE_ROOT, 'node_modules/.bin/tsc')], { cwd: PACKAGE_ROOT });
 			if (result.stdout.length) console.log(result.stdout.toString());
 			if (result.stderr.length) console.log(result.stderr.toString());
-			let pid_path = join(tmpdir(), 'harperdb-tsc.pid');
-			let is_running;
-			if (existsSync(pid_path)) {
-				try {
-					// signal of zero can be used to check for existence
-					process.kill(+readFileSync(pid_path, { encoding: 'utf8' }), 0);
-					is_running = true;
-				} catch (e) {}
-			}
-			if (!is_running) {
-				console.log('starting tsc background process');
-				let tsc_process = spawn(process.argv[0], [join(PACKAGE_ROOT, 'node_modules/.bin/tsc'), '--watch'], {
-					cwd: PACKAGE_ROOT,
-					detached: true,
-					stdio: 'ignore',
-				});
-				writeFileSync(pid_path, tsc_process.pid.toString());
-				tsc_process.unref();
+			if (target_directory_existed) {
+				let pid_path = join(tmpdir(), 'harperdb-tsc.pid');
+				let is_running;
+				if (existsSync(pid_path)) {
+					try {
+						// signal of zero can be used to check for existence
+						process.kill(+readFileSync(pid_path, { encoding: 'utf8' }), 0);
+						is_running = true;
+					} catch (e) {}
+				}
+				if (!is_running) {
+					console.log('starting tsc background process');
+					let tsc_process = spawn(process.argv[0], [join(PACKAGE_ROOT, 'node_modules/.bin/tsc'), '--watch'], {
+						cwd: PACKAGE_ROOT,
+						detached: true,
+						stdio: 'ignore',
+					});
+					writeFileSync(pid_path, tsc_process.pid.toString());
+					tsc_process.unref();
+				}
 			}
 		}
 	}
