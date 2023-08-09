56ed594c1e026c9bc8ad125abd8f38d4f3a359ea
Kris Zyp <kriszyp@gmail.com>
2023-08-09 05:47:51 -0600

    More comments

--- resources/transaction.ts
+++ resources/transaction.ts
@@ -25,18 +25,20 @@ export function transaction<T>(
 	const transaction = (context.transaction = new TransactionSet());
 	if (context.timestamp) transaction.timestamp = context.timestamp;
 	transaction[CONTEXT] = context;
+	// create a resource cache so that multiple requests to the same resource return the same resource
 	context.resourceCache = [];
 	let result;
 	try {
 		result = callback(transaction);
 		if (result?.then) {
-			return result.then(onSuccess, onError);
+			return result.then(onComplete, onError);
 		}
 	} catch (error) {
 		onError(error);
 	}
-	return onSuccess(result);
-	function onSuccess(result) {
+	return onComplete(result);
+	// when the transaction function completes, run this to commit the transaction
+	function onComplete(result) {
 		const committed = transaction.commit();
 		if (committed.then) {
 			return committed.then(() => {
@@ -48,6 +50,7 @@ export function transaction<T>(
 			return result;
 		}
 	}
+	// if the transaction function throws an error, we abort
 	function onError(error) {
 		transaction.abort();
 		context.transaction = null;
@@ -82,6 +85,8 @@ class TransactionSet extends Array {
 			const txn = this[i];
 			txn.validate?.();
 		}
+		// if there was no context-provided timestamp, we set the transaction timestamp now, this helps to
+		// ensure that any writes have a greater timestamp than any data that was read in the transaction
 		if (!this.timestamp) this.timestamp = getNextMonotonicTime();
 		for (let i = 0; i < l; i++) {
 			const txn = this[i];
