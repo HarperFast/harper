7f9b23d68eb9e2e387e7550ef954ec205ea9d2be
Kris Zyp <kriszyp@gmail.com>
2023-10-12 08:38:59 -0600

    Make sure to kill pm2 before starting

--- test/ci-tests/source-code-unit-tests.sh
+++ test/ci-tests/source-code-unit-tests.sh
@@ -7,11 +7,10 @@ set -e
 npm --loglevel=error install mocha -g
 cd /home/ubuntu/harperdb/bin/
 node harperdb.js stop
-ps -ef | grep pm2 || echo "no ps?"
 npm run cover:test
-ps -ef | grep pm2 || echo "no ps?"
+
 npx pm2 kill
-ps -ef | grep pm2 || echo "no ps?"
+
 
 # Re-register HarperDB after unit tests
 cd /home/ubuntu/harperdb/utility/devops
@@ -19,5 +18,4 @@ node register.js --reset_license --ram_allocation=16384
 
 ps -ef | grep pm2 || echo "no ps?"
 cd /home/ubuntu/harperdb/bin/
-node harperdb.js &
-sleep 5
+node harperdb.js start
