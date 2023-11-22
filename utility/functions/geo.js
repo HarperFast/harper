702d90683fdb1b9d5d5ae23f11f02040cb423a84
DavidCockerill <david@harperdb.io>
2023-11-22 10:13:49 -0500

    CORE-2295 update unit tests

--- utility/functions/geo.js
+++ utility/functions/geo.js
@@ -18,7 +18,6 @@ const turf_booleanDisjoint = require('@turf/boolean-disjoint');
 const turf_helpers = require('@turf/helpers');
 const hdb_terms = require('../hdbTerms');
 const common_utils = require('../common_utils');
-const hdb_log = require('../logging/harper_logger');
 
 module.exports = {
 	geoArea: geoArea,
@@ -156,12 +155,10 @@ function geoDistance(point1, point2, units) {
  */
 function geoNear(point1, point2, distance, units) {
 	if (common_utils.isEmpty(point1)) {
-		hdb_log.warn('geoNear received null or undefined value for point 1');
 		return false;
 	}
 
 	if (common_utils.isEmpty(point2)) {
-		hdb_log.warn('geoNear received null or undefined value for point 2');
 		return false;
 	}
 
