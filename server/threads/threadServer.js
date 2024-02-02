f058c8e86faa29e3a07b8df952b849afd6ddfaa4
Kris Zyp <kriszyp@gmail.com>
2024-02-01 17:38:58 -0700

    Close inspector when trying to restart threads

--- server/threads/threadServer.js
+++ server/threads/threadServer.js
@@ -144,7 +144,7 @@ function startServers() {
 								}, 5000).unref();
 							});
 						}
-						if (process.env.DEV_MODE) {
+						if (debug_threads || process.env.DEV_MODE) {
 							try {
 								require('inspector').close();
 							} catch (error) {
