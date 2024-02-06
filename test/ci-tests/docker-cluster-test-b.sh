304c40da791e7f6969ddc6b3eae8f9b2da76b8c8
Mathew Branyon <mat@harperdb.io>
2024-02-06 12:40:22 -0700

    [no ci] workflow updates

--- test/ci-tests/docker-cluster-test-b.sh
+++ test/ci-tests/docker-cluster-test-b.sh
@@ -27,10 +27,10 @@ sed -in "s/ubuntu/harperdb/" integrationTests/clusterTests/clusterTestB/Four_Nod
 
 # create output directories for test
 artifact_dir="artifact"
-mkdir --mode a+rwx -p $artifact_dir/ClstrTestB1/newman
-mkdir --mode a+rwx -p $artifact_dir/ClstrTestB2/
-mkdir --mode a+rwx -p $artifact_dir/ClstrTestB3/
-mkdir --mode a+rwx -p $artifact_dir/ClstrTestB4/
+mkdir --mode a+rwx -p $artifact_dir/ClstrTestB1/newman $artifact_dir/ClstrTestB1/log
+mkdir --mode a+rwx -p $artifact_dir/ClstrTestB2/ $artifact_dir/ClstrTestB2/log
+mkdir --mode a+rwx -p $artifact_dir/ClstrTestB3/ $artifact_dir/ClstrTestB3/log
+mkdir --mode a+rwx -p $artifact_dir/ClstrTestB4/ $artifact_dir/ClstrTestB4/log
 
 # create network
 sudo docker network create ClstrTestB
@@ -39,7 +39,7 @@ sudo docker network create ClstrTestB
 docker_args="-d --restart no --network ClstrTestB -e HDB_ADMIN_USERNAME=admin -e HDB_ADMIN_PASSWORD=Abc1234! -e CLUSTERING_PORT=12345 -e CLUSTERING_HUBSERVER_CLUSTER_NETWORK_PORT=12345 -e CLUSTERING_USER=cluster_user -e CLUSTERING_PASSWORD=Abc1234! -e CLUSTERING=true -e HARPERDB_FINGERPRINT=\"4sADRM2e7dd501d7db58bb02d35bd0745146423a1\" -e HARPERDB_LICENSE='{\"license_key\":\"1373969b4cabd39e8b29c3c16e419c6abef4437d48168b4721e575610dcf656cf57016bd037cf1107f8232a1e94b7352c05b7c069560ac0e54ca156d3babc966289c664e5fb163e3429158a2f57afd39854ca51fa885abbca62f7e063a334498cd5ef0a038cbea80c6e063d174bbcc189a81dc4f2771ad90c9022cf20a322043ec4213fdef1d1e9eba36117c865acb6a13be3be218513ee80385e78bda0b9e27e7ec6532ac6e5416bc020f4f4be91cf6mofi25eYMW6aiRf6bd3d9b691699749569f6acd34a93e61\",\"company\":\"harperdb.io\"}' -e LOG_TO_STDSTREAMS=true -e LOGGING_LEVEL=trace -e LOG_TO_FILE=true -e CLUSTERING_REPUBLISHMESSAGES=true -e CLUSTERING_LOGLEVEL=info ${docker_image}:${harperdb_version}"
 
 # launch test containers
-sudo docker run --name ClstrTestB1 -e NODE_NAME=ClstrTestB1 -v `pwd`/integrationTests/:/home/harperdb/harperdb -v `pwd`/$artifact_dir/newman/:/home/harperdb/harperdb/integrationTests/newman -v `pwd`/$artifact_dir/ClstrTestB1/log:/home/harperdb/hdb/log ${docker_args}
+sudo docker run --name ClstrTestB1 -e NODE_NAME=ClstrTestB1 -v `pwd`/integrationTests/:/home/harperdb/harperdb/integrationTests/ -v `pwd`/$artifact_dir/newman/:/home/harperdb/harperdb/integrationTests/newman -v `pwd`/$artifact_dir/ClstrTestB1/log:/home/harperdb/hdb/log ${docker_args}
 sudo docker run --name ClstrTestB2 -e NODE_NAME=ClstrTestB2 -v `pwd`/test/:/home/harperdb/harperdb/test:z -v `pwd`/$artifact_dir/ClstrTestB2/log:/home/harperdb/hdb/log ${docker_args}
 sudo docker run --name ClstrTestB3 -e NODE_NAME=ClstrTestB3 -v `pwd`/test/:/home/harperdb/harperdb/test:z -v `pwd`/$artifact_dir/ClstrTestB3/log:/home/harperdb/hdb/log ${docker_args}
 sudo docker run --name ClstrTestB4 -e NODE_NAME=ClstrTestB4 -v `pwd`/test/:/home/harperdb/harperdb/test:z -v `pwd`/$artifact_dir/ClstrTestB4/log:/home/harperdb/hdb/log ${docker_args}
