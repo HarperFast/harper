21ebeb66d8dcbe65c4b6a9b288fdd7f522062085
DavidCockerill <david@harperdb.io>
2024-02-27 13:20:10 -0500

    CORE-2329 Add config option(s) for compression dictionary

--- utility/lmdb/OpenDBIObject.js
+++ utility/lmdb/OpenDBIObject.js
@@ -2,10 +2,21 @@
 const env_mngr = require('../environment/environmentManager');
 const terms = require('../../utility/hdbTerms');
 const { RecordEncoder } = require('../../resources/RecordEncoder');
+const fs = require('fs');
 env_mngr.initSync();
+
 const LMDB_COMPRESSION = env_mngr.get(terms.CONFIG_PARAMS.STORAGE_COMPRESSION);
+const STORAGE_COMPRESSION_DICTIONARY = env_mngr.get(terms.CONFIG_PARAMS.STORAGE_COMPRESSION_DICTIONARY);
+const STORAGE_COMPRESSION_THRESHOLD = env_mngr.get(terms.CONFIG_PARAMS.STORAGE_COMPRESSION_THRESHOLD);
+let LMDB_COMPRESSION_OPTS;
+if (STORAGE_COMPRESSION_DICTIONARY && STORAGE_COMPRESSION_THRESHOLD) {
+	LMDB_COMPRESSION_OPTS = {
+		dictionary: fs.readFileSync(STORAGE_COMPRESSION_DICTIONARY),
+		threshold: STORAGE_COMPRESSION_THRESHOLD,
+	};
+}
 const LMDB_CACHING = env_mngr.get(terms.CONFIG_PARAMS.STORAGE_CACHING) !== false;
-const UPDATES_PROPERTY = terms.UPDATES_PROPERTY;
+
 /**
  * Defines how a DBI will be created/opened
  */
@@ -18,19 +29,21 @@ class OpenDBIObject {
 		this.dupSort = dup_sort === true;
 		this.encoding = dup_sort ? 'ordered-binary' : 'msgpack';
 		this.useVersions = is_primary;
-		this.compression = LMDB_COMPRESSION &&
-			is_primary && {
-				startingOffset: 32, // don't compress headers
-			};
+		if (LMDB_COMPRESSION_OPTS) {
+			this.compression = LMDB_COMPRESSION_OPTS;
+		} else {
+			this.compression = LMDB_COMPRESSION &&
+				is_primary && {
+					startingOffset: 32, // don't compress headers
+				};
+		}
+
 		this.sharedStructuresKey = Symbol.for('structures');
 		if (is_primary) {
 			this.cache = LMDB_CACHING && { validated: true };
 			this.randomAccessStructure = true;
 			this.freezeData = true;
 			this.encoder = { Encoder: RecordEncoder };
-			this.alwaysLazyProperty = (property) => {
-				return property === UPDATES_PROPERTY;
-			};
 		}
 	}
 }
