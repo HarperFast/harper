742614c92e07d66a0ff0babac5b67fb44640c117
Kris Zyp <kriszyp@gmail.com>
2024-04-16 09:52:20 -0600

    Keep compaction transactions open so it doesn't timeout

--- bin/copyDb.ts
+++ bin/copyDb.ts
@@ -122,7 +122,9 @@ export async function copyDb(source_database: string, target_database_path: stri
 	const target_dbis_db = target_env.openDB(INTERNAL_DBIS_NAME);
 	let written;
 	let outstanding_writes = 0;
-	for (const { key, value: attribute } of source_dbis_db.getRange({})) {
+	// we use a single transaction to get a snapshot, also we can't use snapshot: false on dupsort dbs
+	const transaction = source_dbis_db.useReadTransaction();
+	for (const { key, value: attribute } of source_dbis_db.getRange({ transaction })) {
 		const is_primary = attribute.is_hash_attribute || attribute.isPrimaryKey;
 		let existing_compression, new_compression;
 		if (is_primary) {
@@ -150,17 +152,18 @@ export async function copyDb(source_database: string, target_database_path: stri
 		const target_dbi = target_env.openDB(key, dbi_init);
 		target_dbi.encoder = null;
 		console.log('copying', key, 'from', source_database, 'to', target_database_path);
-		await copyDbi(source_dbi, target_dbi, is_primary);
+		await copyDbi(source_dbi, target_dbi, is_primary, transaction);
 	}
 	const target_audit_store = root_store.openDB(AUDIT_STORE_NAME, AUDIT_STORE_OPTIONS);
 	console.log('copying audit log');
 	copyDbi(source_audit_store, target_audit_store, false);
-	async function copyDbi(source_dbi, target_dbi, is_primary) {
+	async function copyDbi(source_dbi, target_dbi, is_primary, transaction) {
 		let records_copied = 0;
 		let bytes_copied = 0;
-		for (const { key, value, version } of source_dbi.getRange({ start: null, versions: is_primary })) {
+		for (const { key, value, version } of source_dbi.getRange({ start: null, versions: is_primary, transaction })) {
 			written = target_dbi.put(key, value, version);
 			records_copied++;
+			if (transaction.openTimer) transaction.openTimer = 0; // reset the timer, don't want it to time out
 			bytes_copied += (key.length || 10) + value.length;
 			if (outstanding_writes++ > 5000) {
 				await written;
