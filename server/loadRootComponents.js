51e7aa18a72d4210b98c40c868335805a67c2ef8
Kris Zyp <kriszyp@gmail.com>
2024-04-26 08:24:46 -0600

    Load certificates into hdb_certificate table

--- server/loadRootComponents.js
+++ server/loadRootComponents.js
@@ -1,5 +1,5 @@
 const { isMainThread } = require('worker_threads');
-const { getTables } = require('../resources/databases');
+const { getTables, table } = require('../resources/databases');
 const { loadComponentDirectories, loadComponent } = require('../components/componentLoader');
 const { resetResources } = require('../resources/Resources');
 const install_components = require('../components/installComponents');
@@ -8,6 +8,8 @@ const { dirname } = require('path');
 const { getConnection } = require('./nats/utility/natsUtils');
 const env_mgr = require('../utility/environment/environmentManager');
 const hdb_terms = require('../utility/hdbTerms');
+const { CONFIG_PARAMS } = require('../utility/environment/environmentManager');
+const { readFileSync } = require('fs');
 
 let loaded_components = new Map();
 /**
@@ -16,7 +18,7 @@ let loaded_components = new Map();
  */
 async function loadRootComponents(is_worker_thread = false) {
 	// Create and cache the nats client connection
-	if (!isMainThread && env_mgr.get(hdb_terms.CONFIG_PARAMS.CLUSTERING_ENABLED)) {
+	if (!isMainThread && env_mgr.get(CONFIG_PARAMS.CLUSTERING_ENABLED)) {
 		// The await is purposely omitted here so that is doesnt slow down startup time
 		getConnection();
 	}
@@ -29,6 +31,7 @@ async function loadRootComponents(is_worker_thread = false) {
 	let resources = resetResources();
 	getTables();
 	resources.isWorker = is_worker_thread;
+	await loadCertificates();
 	// the HarperDB root component
 	await loadComponent(dirname(config_utils.getConfigFilePath()), resources, 'hdb', true, loaded_components);
 	// once the global plugins are loaded, we now load all the CF and run applications (and their components)
@@ -39,5 +42,43 @@ async function loadRootComponents(is_worker_thread = false) {
 	}
 	if (all_ready.length > 0) await Promise.all(all_ready);
 }
-
+const CERTIFICATE_CONFIGS = [
+	CONFIG_PARAMS.TLS_CERTIFICATE,
+	CONFIG_PARAMS.TLS_CERTIFICATEAUTHORITY,
+	CONFIG_PARAMS.OPERATIONSAPI_TLS_CERTIFICATE,
+	CONFIG_PARAMS.OPERATIONSAPI_TLS_CERTIFICATEAUTHORITY,
+];
+function loadCertificates() {
+	const certificate_table = table({
+		table: 'hdb_certificate',
+		database: 'system',
+		attributes: [
+			{
+				name: 'name',
+				isPrimaryKey: true,
+			},
+			{
+				attribute: 'uses',
+			},
+			{
+				attribute: 'certificate',
+			},
+			{
+				attribute: 'is_authority',
+			},
+		],
+	});
+	let promise;
+	for (let config_key of CERTIFICATE_CONFIGS) {
+		const path = env.get(config_key);
+		if (path) {
+			promise = certificate_table.put({
+				name: config_key,
+				certificate: readFileSync(path),
+				is_authority: config_key.includes('uthority'),
+			});
+		}
+	}
+	return promise;
+}
 module.exports.loadRootComponents = loadRootComponents;
