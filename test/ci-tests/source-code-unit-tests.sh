b4ec2d026bc2b0914855b35981f2d61681b3032e
DavidCockerill <david@harperdb.io>
2024-07-29 10:21:10 -0400

    change how HDB is installed and started

--- test/ci-tests/source-code-unit-tests.sh
+++ test/ci-tests/source-code-unit-tests.sh
@@ -11,14 +11,9 @@ npm run cover:test
 
 npx pm2 kill
 
-
-# Re-register HarperDB after unit tests
-cd /home/ubuntu/harperdb/utility/devops
-node register.js --reset_license --ram_allocation=16384
-
 cd /home/ubuntu/harperdb
 sudo chmod +x ./utility/devops/build/build-studio.sh
 ./utility/devops/build/build-studio.sh
 
 cd /home/ubuntu/harperdb/bin/
-node harperdb.js start
+nohup node harperdb.js &
