feb34095aa78b58b3414206f0d106bd140ae5299
Kris Zyp <kriszyp@gmail.com>
2024-10-01 22:30:48 -0600

    Remove query parsing to array ids

--- resources/search.ts
+++ resources/search.ts
@@ -1093,12 +1093,6 @@ function decodeProperty(name) {
 function typedDecoding(value) {
 	// for non-strict operators, we allow for coercion of types
 	if (value === 'null') return null;
-	if (value.indexOf('/') > -1) {
-		const parts = value.split('/');
-		const multi_part = new MultiPartId(parts.length);
-		for (let i = 0, l = parts.length; i < l; i++) multi_part[i] = typedDecoding(parts[i]);
-		return multi_part;
-	}
 	if (value.indexOf(':') > -1) {
 		const [type, value_to_coerce] = value.split(':');
 		if (type === 'number') {
