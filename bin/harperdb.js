5c2bdc9dca319f16947f16b373503de4f8aaa717
Ethan Arrowood <ethan@arrowood.dev>
2024-10-22 10:31:37 -0600

    fix help message

--- bin/harperdb.js
+++ bin/harperdb.js
@@ -21,6 +21,8 @@ Usage: harperdb [command]
 
 With no command, harperdb will simply run HarperDB (in the foreground)
 
+By default, the CLI also supports all Operation APIs. Specify the operation name and any required parameters, and omit the 'operation' command.
+
 Commands:
 copy-db <source> <target>       - Copies a database from source path to target path
 dev <path>                      - Run the application in dev mode with debugging, foreground logging, no auth
