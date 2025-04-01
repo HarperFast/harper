a3cb0cac5ba30a1fd2930646657e8c0517750a38
Wes Morgan <git@wesmorgan.me>
2025-04-01 13:36:53 -0600

    Use current API for get & search in analytics/read

--- resources/analytics/read.ts
+++ resources/analytics/read.ts
@@ -1,25 +1,16 @@
-import type { SearchByConditionsRequest } from '../../dataLayer/harperBridge/ResourceBridge';
-import search from '../../dataLayer/search';
 import type { Metric } from './write';
 import { loggerWithTag } from '../../utility/logging/harper_logger';
 import { getAnalyticsHostnameTable } from './hostnames';
+import type { Resource } from 'harperdb';
+import type { Conditions } from '../ResourceInterface';
 
 const log = loggerWithTag('analytics');
 
-async function lookupHostname(nodeId: number): Promise<string|null> {
-	const searchByIdReq = {
-		database: 'system',
-		table: getAnalyticsHostnameTable().name,
-		ids: [nodeId],
-		get_attributes: ['*'],
-	};
-	log.trace?.(`lookupHostname searchByIdReq: ${JSON.stringify(searchByIdReq)}`);
-	const results = await search.searchByHash(searchByIdReq);
-	for await (const result of results) {
-		log.trace?.(`lookupHostname result: ${JSON.stringify(result)}`);
-		return result.hostname;
-	}
-	return null; // OK to return null here b/c this is just a user convenience thing; should find a hostname eventually
+type AnalyticsHostnameResource = Resource & { hostname: string };
+
+async function lookupHostname(nodeId: number): Promise<string> {
+	const result: AnalyticsHostnameResource = await getAnalyticsHostnameTable().get(nodeId);
+	return result.hostname;
 }
 
 interface AnalyticsRequest {
@@ -30,28 +21,26 @@ interface AnalyticsRequest {
 }
 
 export async function get(req: AnalyticsRequest): Promise<Metric[]> {
-	const searchByConditionsReq: SearchByConditionsRequest = {
-		database: 'system',
-		table: 'hdb_analytics',
-		conditions: [{ search_attribute: 'metric', search_type: 'equals', search_value: req.metric }],
-		get_attributes: req.getAttributes ? req.getAttributes : ['*'],
-	};
+	log.trace?.('get analytics request received');
+	const conditions: Conditions = [{ attribute: 'metric', comparator: 'equals', value: req.metric }];
+	const select = req.getAttributes ? req.getAttributes : ['*'];
 	if (req.startTimestamp) {
-		searchByConditionsReq.conditions!.push({
-			search_attribute: 'id',
-			search_type: 'greater_than_equal',
-			search_value: req.startTimestamp,
+		conditions.push({
+			attribute: 'id',
+			comparator: 'greater_than_equal',
+			value: req.startTimestamp,
 		});
 	}
 	if (req.endTimestamp) {
-		searchByConditionsReq.conditions!.push({
-			search_attribute: 'id',
-			search_type: 'less_than',
-			search_value: req.endTimestamp,
+		conditions.push({
+			attribute: 'id',
+			comparator: 'less_than',
+			value: req.endTimestamp,
 		});
 	}
-	log.trace?.(`get searchByConditionsReq: ${JSON.stringify(searchByConditionsReq)}`);
-	const searchResults = await search.searchByConditions(searchByConditionsReq);
+	const request = { conditions, select };
+	log.trace?.("get search request:", request);
+	const searchResults = await databases.system.hdb_analytics.search(request);
 	const results: Metric[] = [];
 	for await (const result of searchResults) {
 		log.trace?.(`get result: ${JSON.stringify(result)}`);
