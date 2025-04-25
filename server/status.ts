6bb33830240deea8b61ac36e89cf7e19d83517c6
Nathan Heskew <nathan@harperdb.io>
2025-04-25 06:52:03 -0700

    use logger tag

--- server/status.ts
+++ server/status.ts
@@ -1,6 +1,6 @@
 import { table } from '../resources/databases.js';
 import { Id } from '../resources/ResourceInterface.js';
-import * as logger from '../utility/logging/logger.js';
+import { loggerWithTag } from '../utility/logging/logger.js';
 import { OperationRequestBody } from './operationsServer.js';
 
 export {
@@ -12,8 +12,11 @@ export {
 const STATUS_DEFAULT = 'primary';
 
 type StatusOperationRequestBody = OperationRequestBody & {
-    id?: Id;
-    status?: string;
+    id: Id;
+};
+
+type StatusOperationWriteRequestBody = StatusOperationRequestBody & {
+    status: string;
 };
 
 const Status = table({
@@ -36,15 +39,15 @@ const Status = table({
     ],
 });
 
+const statusLogger = loggerWithTag('status');
+
 function clearStatus({ id }: StatusOperationRequestBody) {
-    // todo: not allow clearing default status?
-    // todo: throw if the status doesn't exist?
-    logger.debug?.('clearStatus', id);
+    statusLogger.debug?.('clearStatus', id);
     return Status.delete(id);
 }
 
 function getAllStatus() {
-    logger.debug?.('getAllStatus');
+    statusLogger.debug?.('getAllStatus');
     return Status.get({});
 }
 
@@ -52,17 +55,17 @@ function getAllStatus() {
 // todo: is there a reason to get a single status?
 function getStatus({ id }: StatusOperationRequestBody) {
     if (!id) {
-        logger.debug?.('getStatus', 'all');
+        statusLogger.debug?.('getStatus', 'all');
         return getAllStatus();
     }
 
-    logger.debug?.('getStatus', id);
+    statusLogger.debug?.('getStatus', id);
     return Status.get(id);;
 }
 
-function setStatus({ status, id = STATUS_DEFAULT }: StatusOperationRequestBody) {
+function setStatus({ status, id = STATUS_DEFAULT }: StatusOperationWriteRequestBody) {
     // todo: validate
     // todo: return all, or just this status, or just success...or just the put operation
-    logger.debug?.('setStatus', id, status);
+    statusLogger.debug?.('setStatus', id, status);
     return Status.put(id, { status });
 }
