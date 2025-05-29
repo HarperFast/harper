0edd9473bfc9ddbea5d5a013ce18043684140c45
Ethan Arrowood <ethan@arrowood.dev>
2025-05-29 17:34:25 -0600

    adjust warning message

--- components/Component.ts
+++ components/Component.ts
@@ -119,7 +119,7 @@ export class Component {
 
 			if (pattern.startsWith('/')) {
 				harperLogger.warn(
-					`Leading '/' in 'files' glob pattern is deprecated. For backwards compatibility purposes, it is currently transformed to the relative path of the component, but in the future will result in an error. Please replace with a relative path such as './' or removing the leading path separator all together ('./web/*' -> 'web/*').`
+					`Leading '/' in 'files' glob pattern is deprecated. For backwards compatibility purposes, it is currently transformed to the relative path of the component, but in the future will result in an error. Paths are automatically derived from the root of the component directory. Please remove (i.e. '/web/*' -> 'web/*').`
 				);
 
 				pattern = pattern === '/' ? '.' : pattern.slice(1);
