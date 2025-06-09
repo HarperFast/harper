6e006765f6d80bf42e69187b67c81c19b95e9550
Nathan Heskew <nathan@harperdb.io>
2025-06-09 12:52:11 -0700

    inject some secrets...

--- test/ci-tests/cluster-test-a.sh
+++ test/ci-tests/cluster-test-a.sh
@@ -27,4 +27,8 @@ sed -in "s/ClstrTestANode3/${private_dns_names[2]%%.*}/" clusterTests/clusterTes
 sed -in "s/ClstrTestANode4/${private_dns_names[3]%%.*}/" clusterTests/clusterTestA/Five_node_cluster_tests_env_var.postman_environment.json
 sed -in "s/ClstrTestANode5/${private_dns_names[4]%%.*}/" clusterTests/clusterTestA/Five_node_cluster_tests_env_var.postman_environment.json
 
+# Inject credentials from environment variables
+sed -i "s/\"value\": \"PLACEHOLDER_USERNAME\"/\"value\": \"${HDB_ADMIN_USERNAME}\"/" clusterTests/clusterTestA/Five_node_cluster_tests_env_var.postman_environment.json
+sed -i "s/\"value\": \"PLACEHOLDER_PASSWORD\"/\"value\": \"${HDB_ADMIN_PASSWORD}\"/" clusterTests/clusterTestA/Five_node_cluster_tests_env_var.postman_environment.json
+
 newman run clusterTests/clusterTestA/Five_Node_Cluster.postman_collection.json -e clusterTests/clusterTestA/Five_node_cluster_tests_env_var.postman_environment.json --reporters cli,html,htmlextra --reporter-html-export newman/report.html --reporter-htmlextra-export newman/extra_report.html  --delay-request 2000 --insecure --reporter-cli-show-timestamps
