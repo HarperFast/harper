aa0bdcb9b1e44891c9a5bd635d25145f6a642951
Nathan Heskew <nathan@harperdb.io>
2025-06-09 10:44:12 -0700

    use hdb admin/pwd secrets in tests

--- test/ci-tests/ci-tests-node-api-tests.sh
+++ test/ci-tests/ci-tests-node-api-tests.sh
@@ -8,4 +8,4 @@ cd /home/ubuntu/harperdb/integrationTests/apiTests
 
 npm install
 
-S3_KEY=$1 S3_SECRET=$2 node --test-reporter spec --test-reporter-destination report.txt --test-reporter spec --test-reporter-destination stdout --experimental-default-type="module" tests/testSuite.js
+HDB_ADMIN_USERNAME=$3 HDB_ADMIN_PASSWORD=$4 S3_KEY=$1 S3_SECRET=$2 node --test-reporter spec --test-reporter-destination report.txt --test-reporter spec --test-reporter-destination stdout --experimental-default-type="module" tests/testSuite.js
