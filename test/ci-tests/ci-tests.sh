b852bf6c229579f460d55bf9ded56b55ede1cebf
Nathan Heskew <nathan@harperdb.io>
2025-06-09 12:42:08 -0700

    start to get some secrets out of postman configs. hdb creds are for good practice, s3 a little better practice

--- test/ci-tests/ci-tests.sh
+++ test/ci-tests/ci-tests.sh
@@ -15,6 +15,13 @@ cd /home/ubuntu/harperdb/integrationTests
 
 # Correct path to CSV files for integration tests
 sed -i 's/\/usr\/csv\//\/home\/ubuntu\/harperdb\/test\/data\/integrationTestsCsvs\//g' Int_test_env_var.json
+
+# Inject credentials from environment variables
+sed -i "s/\"value\": \"PLACEHOLDER_USERNAME\"/\"value\": \"${HDB_ADMIN_USERNAME}\"/" Int_test_env_var.json
+sed -i "s/\"value\": \"PLACEHOLDER_PASSWORD\"/\"value\": \"${HDB_ADMIN_PASSWORD}\"/" Int_test_env_var.json
+sed -i "s/\"value\": \"PLACEHOLDER_S3_KEY\"/\"value\": \"${S3_KEY}\"/" Int_test_env_var.json
+sed -i "s/\"value\": \"PLACEHOLDER_S3_SECRET\"/\"value\": \"${S3_SECRET}\"/" Int_test_env_var.json
+
 cat Int_test_env_var.json | grep "integrationTestsCsvs"
 
 newman run HarperDB_Integration_Tests.json -e Int_test_env_var.json --timeout-request 30000 --reporters cli,html,htmlextra --reporter-html-export newman/report.html --reporter-htmlextra-export newman/extra_report.html --insecure --reporter-cli-show-timestamps || gdb node /var/lib/apport/coredump/* --batch -ex=backtrace || gdb node /home/ubuntu/*core* --batch -ex=backtrace
