28429114fedfe487ee178c300ddd0b99253c9e66
Wes Morgan <git@wesmorgan.me>
2025-06-13 11:34:15 -0600

    Manually add 'node' attr to list returned by describeMetric

--- resources/analytics/read.ts
+++ resources/analytics/read.ts
@@ -168,6 +168,8 @@ export async function describeMetric(metric: string): Promise<DescribeMetricResp
 		limit: 1,
 	};
 	const results = databases.system.hdb_analytics.search(lastEntrySearch);
+	// node is a synthetic attribute, so make sure it's included
+	const attributes = [{ name: 'node', type: 'string' }];
 	for await (const result of results) {
 		for (const attr in result) {
 			attributes.push({ name: attr, type: typeof result[attr] });
