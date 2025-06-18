481f4d05f6d3ba7f1facd751a4880176bedca19a
Mihai <mihai@harperdb.io>
2025-06-18 13:37:01 +0300

    ui tests workflow

---
+++ test/ci-tests/ci-tests-ui-tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+. /home/ubuntu/.nvm/nvm.sh
+. /home/ubuntu/.nvm/bash_completion
+
+pwd
+ls -al /home/ubuntu/
+cd /home/ubuntu/ui-tests/
+
+echo "npm install..."
+npm install
+
+echo "Install Playwright Browsers"
+npx playwright install --with-deps
+
+echo "Run Playwright tests"
+npx playwright test
+
