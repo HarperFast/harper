3ee9395c80ff722e7da7e9b59486a2094d934fef
Wes Morgan <git@wesmorgan.me>
2025-08-07 14:02:04 -0600

    Fix docs & another reference to testSuite.js

--- test/ci-tests/ci-tests-node-api-tests.sh
+++ test/ci-tests/ci-tests-node-api-tests.sh
@@ -8,4 +8,4 @@ cd /home/ubuntu/harperdb/integrationTests/apiTests
 
 npm install
 
-HDB_ADMIN_USERNAME=$1 HDB_ADMIN_PASSWORD=$2 S3_KEY=$3 S3_SECRET=$4 node --test-reporter spec --test-reporter-destination report.txt --test-reporter spec --test-reporter-destination stdout --experimental-default-type="module" tests/testSuite.js
+HDB_ADMIN_USERNAME=$1 HDB_ADMIN_PASSWORD=$2 S3_KEY=$3 S3_SECRET=$4 node --test-reporter spec --test-reporter-destination report.txt --test-reporter spec --test-reporter-destination stdout --experimental-default-type="module" tests/testSuite.mjs
