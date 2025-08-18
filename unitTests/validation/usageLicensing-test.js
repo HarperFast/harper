e79d9309e698674042473f2bef22212adc2a17bc
Wes Morgan <git@wesmorgan.me>
2025-08-18 15:36:50 -0600

    Replace NODE_ENV with LICENSE_MODE
    ...for choosing whether to use a production or dev/test license key

--- unitTests/validation/usageLicensing-test.js
+++ unitTests/validation/usageLicensing-test.js
@@ -1,9 +1,18 @@
-const { describe, it } = require('mocha');
+const { describe, it, before, after } = require('mocha');
 const { expect } = require('chai');
+const sinon = require('sinon');
 const ul = require('../../validation/usageLicensing.ts');
-const { signTestLicense, signAnything } = require('../testLicenseUtils.js');
+const utils = require('../testLicenseUtils.js');
 
 describe('usageLicensing', function () {
+	before(() => {
+		sinon.replace(ul, 'publicKey', new ul.PublicKey('test'));
+	});
+
+	after(() => {
+		sinon.restore();
+	});
+
 	describe('validateLicense', () => {
 		it('should return license payload on valid license', () => {
 			const payload = {
@@ -20,7 +29,7 @@ describe('usageLicensing', function () {
 				storage: -1,
 				expiration: '2030-01-01T00:00:00.000Z',
 			};
-			const license = signTestLicense(payload);
+			const license = utils.signTestLicense(payload);
 			const result = ul.validateLicense(license);
 			expect(result).to.deep.equal(payload);
 		});
@@ -32,7 +41,7 @@ describe('usageLicensing', function () {
 		});
 
 		it('should throw InvalidBase64UrlEncodingError on invalid base64url encoding', () => {
-			expect(() => ul.validateLicense(signAnything('foo!.$bar'))).to.throw(TypeError);
+			expect(() => ul.validateLicense(utils.signAnything('foo!.$bar'))).to.throw(TypeError);
 		});
 
 		it('should throw InvalidLicenseError if more than three dot-seperated sections', () => {
@@ -49,14 +58,14 @@ describe('usageLicensing', function () {
 		it('should throw InvalidHeaderError on non-JSON license header', () => {
 			const invalidHeader = Buffer.from(JSON.stringify({ foo: 'bar' }) + 'oops!').toString('base64url');
 			const payload = Buffer.from('invalid').toString('base64url');
-			const invalidLicense = signAnything(invalidHeader + '.' + payload);
+			const invalidLicense = utils.signAnything(invalidHeader + '.' + payload);
 			expect(() => ul.validateLicense(invalidLicense)).to.throw(ul.InvalidHeaderError);
 		});
 
 		it('should throw InvalidLicensePayloadError on non-JSON license payload', () => {
 			const header = Buffer.from(JSON.stringify({ typ: 'Harper-License', alg: 'EdDSA' })).toString('base64url');
 			const invalidPayload = Buffer.from(JSON.stringify({ ima: 'payload' }) + 'oops!').toString('base64url');
-			const invalidLicense = signAnything(header + '.' + invalidPayload);
+			const invalidLicense = utils.signAnything(header + '.' + invalidPayload);
 			expect(() => ul.validateLicense(invalidLicense)).to.throw(ul.InvalidPayloadError);
 		});
 	});
