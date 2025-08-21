403fd43370aadb7e0365dd7b054fdbf1d60cc99e
Kris Zyp <kriszyp@gmail.com>
2025-08-21 12:59:59 -0600

    Make sure to return the iterator result in return method

--- resources/Table.ts
+++ resources/Table.ts
@@ -2184,11 +2184,11 @@ export function makeTable(options) {
 						},
 						return() {
 							if (results.onDone) results.onDone();
-							dbIterator.return();
+							return dbIterator.return();
 						},
 						throw() {
 							if (results.onDone) results.onDone();
-							dbIterator.throw();
+							return dbIterator.throw();
 						},
 					};
 				};
