fbb4b37b92696be1905a10c48583025765519437
Ethan Arrowood <ethan@arrowood.dev>
2025-09-04 13:14:39 -0600

    fix removeNodeValidator test

--- unitTests/validation/clustering/removeNodeValidator-test.js
+++ unitTests/validation/clustering/removeNodeValidator-test.js
@@ -3,9 +3,12 @@
 const chai = require('chai');
 const { expect } = chai;
 const removeNodeValidator = require('../../../validation/clustering/removeNodeValidator');
+const envMgr = require('../../../utility/environment/environmentManager');
+const hdbTerms = require('../../../utility/hdbTerms');
 
 describe('Test removeNodeValidator module', () => {
 	it('Test validator returns three errors', () => {
+		envMgr.setProperty(hdbTerms.CONFIG_PARAMS.CLUSTERING_NODENAME, 'node1');
 		const result = removeNodeValidator({
 			operation: true,
 			node_name: 123,
