6bb388a957de0ff4abedaa63be8114532223b4c7
Dawson Toth <dawson@harperdb.io>
2025-09-08 10:23:13 -0400

    Clone and build local studio
    https://harperdb.atlassian.net/browse/STUDIO-369

--- utility/devops/build/build-studio.sh
+++ utility/devops/build/build-studio.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+rm -Rf studio
+mkdir studio
 cd studio
+git clone --branch prod --single-branch --depth 1 https://github.com/HarperDB/hdbms.git .
 npm install -g pnpm
-pnpm
-pnpm run build:local
+pnpm install
+echo "v$(jq -r '.version' ../package.json)"
+VITE_STUDIO_VERSION="v$(jq -r '.version' ../package.json)" pnpm run build:local
 cd ..
