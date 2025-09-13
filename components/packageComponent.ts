f3d51e2ee40c7e1c553bb34cac95e2e6afa0e6d3
ldt1996 <lavinia@harperdb.io>
2025-09-14 01:02:56 +0300

    v4.5- set correct directory permissions for Windows to Linux deployments

--- components/packageComponent.ts
+++ components/packageComponent.ts
@@ -8,18 +8,26 @@ import { createGzip } from 'node:zlib';
  */
 export function packageDirectory(
 	directory: string,
-	options: { skip_node_modules?: boolean, skip_symlinks?: boolean } = { skip_node_modules: false, skip_symlinks: false }
+	options: { skip_node_modules?: boolean; skip_symlinks?: boolean } = { skip_node_modules: false, skip_symlinks: false }
 ): Promise<Buffer> {
 	return new Promise((resolve, reject) => {
-		// for deploy_component to a remote server, we need to tar the local directory
+		// for deployComponent to a remote server, we need to tar the local directory
 		const chunks = [];
 		// pack the directory
 		tar
 			.pack(directory, {
 				dereference: !options.skip_symlinks,
-				ignore: options.skip_node_modules ? (name: string) => {
-					return name.includes('node_modules') || name.includes(join('cache', 'webpack'));
-				} : undefined,
+				ignore: options.skip_node_modules
+					? (name: string) => {
+							return name.includes('node_modules') || name.includes(join('cache', 'webpack'));
+						}
+					: undefined,
+				map: (header) => {
+					if (header.type === 'directory') {
+						header.mode = 0o755;
+					}
+					return header;
+				}
 			})
 			.pipe(createGzip())
 			.on('data', (chunk: Buffer) => chunks.push(chunk))
