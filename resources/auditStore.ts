05e96ab37f4b6463e8f87bd4860c8233283dcf0d
Kris Zyp <kriszyp@gmail.com>
2025-09-22 08:35:53 -0600

    Always remove any blobs referenced by messages, since they can't be shared as a referenced by the record

--- resources/auditStore.ts
+++ resources/auditStore.ts
@@ -177,7 +177,10 @@ export function removeAuditEntry(audit_store: any, key: number, value: any): Pro
 		// if it has blobs, and isn't in use from the main record, we need to delete them as well
 		audit_record = readAuditEntry(value);
 		const primary_store = audit_store.tableStores[audit_record.tableId];
-		const entry = primary_store?.getEntry(audit_record.recordId);
+		const entry =
+			audit_record.type === 'message'
+				? null // if the audit record is a message, then the record won't contain any of the same referenced data, so we should always remove everything
+				: primary_store?.getEntry(audit_record.recordId); // otherwise, we need to check if the record is still in use
 		if (!entry || entry.version !== audit_record.version || !entry.value) {
 			// if the versions don't match or the record has been removed/null-ed, then this should be the only/last reference to any blob
 			decodeFromDatabase(() => deleteBlobsInObject(audit_record.getValue(primary_store)), primary_store.rootStore);
