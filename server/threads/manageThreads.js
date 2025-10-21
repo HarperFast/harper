8920edf5b921eaa14ebe04a753c3639e3e089ac0
Kris Zyp <kriszyp@gmail.com>
2025-10-21 09:52:28 -0600

    Enable heap snapshot before running out of heap memory

--- server/threads/manageThreads.js
+++ server/threads/manageThreads.js
@@ -5,6 +5,7 @@ const { join, isAbsolute, extname } = require('path');
 const { server } = require('../Server.ts');
 const { watch, readdir } = require('fs/promises');
 const { totalmem } = require('os');
+const { setHeapSnapshotNearHeapLimit } = require('v8');
 const hdbTerms = require('../../utility/hdbTerms.ts');
 const envMgr = require('../../utility/environment/environmentManager.js');
 const harperLogger = require('../../utility/logging/harper_logger.js');
@@ -57,6 +58,10 @@ connectedPorts.sendToThread = function (threadId, message) {
 module.exports.whenThreadsStarted = new Promise((resolve) => {
 	module.exports.threadsHaveStarted = resolve;
 });
+
+// make sure this is set on all threads, including the main thread (this is no-op
+// if it was already with the execArgv below)
+setHeapSnapshotNearHeapLimit(1);
 
 let isMainWorker;
 function setTerminateTimeout(newTimeout) {
@@ -139,7 +144,7 @@ function startWorker(path, options = {}) {
 		},
 		execArgv: [
 			'--enable-source-maps',
-
+			'--heapsnapshot-near-heap-limit=1',
 			// expose Node.js internal utils so jsLoader can use `decorateErrorStack()`
 			'--expose-internals',
 		],
