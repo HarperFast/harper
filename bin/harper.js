4e4073e77270398b292b57aebe66badc06856b59
Kris Zyp <kriszyp@gmail.com>
2025-11-12 07:34:22 -0700

    Can't live without source mapping

--- bin/harper.js
+++ bin/harper.js
@@ -9,6 +9,7 @@ const { packageJson } = require('../utility/packageUtils.js');
 const checkNode = require('../launchServiceScripts/utility/checkNodeVersion.js');
 const hdbTerms = require('../utility/hdbTerms.ts');
 const { SERVICE_ACTIONS_ENUM } = hdbTerms;
+process.setSourceMapsEnabled(true); // this is necessary for source maps to work, at least on the main thread.
 
 const HELP = `
 Usage: harperdb [command]
