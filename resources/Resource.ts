7aee8951bba5ed4267ce88e8178e98f7e24c44dc
Kris Zyp <kriszyp@gmail.com>
2026-01-31 20:59:05 -0700

    More carefully define a query as a collection

--- resources/Resource.ts
+++ resources/Resource.ts
@@ -635,10 +635,9 @@ function transactional(action, options) {
 							}
 						}
 					}
-				}
-				if (id === undefined) {
+				} else if (id === undefined) {
 					id = idOrQuery.id ?? null;
-					if (id == null) isCollection = true;
+					if (id == null) query.isCollection = true;
 				}
 			} else {
 				id = idOrQuery;
