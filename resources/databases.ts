b967fbd3759f33d3cb7f7f9dba98ae38a2e97a57
Dawson Toth <dawson@harperdb.io>
2026-02-13 09:13:46 -0500

    refactor: Scope the emitter import
    Co-authored-by: Nathan Heskew <nathan@harperdb.io>

--- resources/databases.ts
+++ resources/databases.ts
@@ -1,4 +1,4 @@
-import { EventEmitter } from 'events';
+import { EventEmitter } from 'node:events';
 import { initSync, getHdbBasePath, get as envGet } from '../utility/environment/environmentManager.js';
 import { INTERNAL_DBIS_NAME } from '../utility/lmdb/terms.js';
 import { open, compareKeys, type Database, type RootDatabase } from 'lmdb';
