ee359f4606cec791cd7436f57c76e8237ec93a76
Kris Zyp <kriszyp@gmail.com>
2026-02-17 14:48:32 -0700

    Properly export more of our top level functions

--- security/jsLoader.ts
+++ security/jsLoader.ts
@@ -391,6 +391,11 @@ function getHarperExports(scope: Scope) {
 		createBlob,
 		RequestTarget,
 		getContext,
+		transaction,
+		getUser,
+		authenticateUser,
+		contentTypes,
+		operation,
 	};
 }
 const ALLOWED_NODE_BUILTIN_MODULES = new Set([
