ceb79c8a318a20618569b359e62d5fb6e8dad5bc
ldt1996 <lavinia@harperdb.io>
2026-05-29 08:19:07 +0300

    fix(analytics): continue instead of return so later dbs still get metrics

--- resources/analytics/write.ts
+++ resources/analytics/write.ts
@@ -335,7 +335,7 @@ function storeDBSizeMetrics(analyticsTable: Table, databases: Databases) {
 			const [firstTable] = Object.values(tables);
 			const dbAuditSize = firstTable?.getAuditSize();
 			if (!dbAuditSize) {
-				return;
+				continue;
 			}
 			let metric;
 			if (firstTable.primaryStore instanceof RocksDatabase) {
@@ -380,7 +380,7 @@ function storeVolumeMetrics(analyticsTable: Table, databases: Databases) {
 			const [firstTable] = Object.values(tables);
 			const storageStats = firstTable?.getStorageStats();
 			if (!storageStats) {
-				return;
+				continue;
 			}
 			const metric = {
 				metric: METRIC.STORAGE_VOLUME,
